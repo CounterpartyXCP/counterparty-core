ea52d362d06b5e45912b4d26efd9c4f79faa1f02
xnova <xnova@bitmessage.ch>
2014-01-19 18:04:03 -0500

    revamped API to allow for arbitrary filtering, via filter objects

--- lib/api.py
+++ lib/api.py
@@ -28,142 +28,72 @@ class reqthread ( threading.Thread ):
         db = apsw.Connection(config.DATABASE)
         db.setrowtrace(util.rowtracer)
 
+        ######################
+        #READ API
         @dispatcher.add_method
         def get_address (address):
             try:
-                return util.get_address(db,
-                    address=address)
+                return util.get_address(db, address=address)
             except exceptions.InvalidAddressError:
                 return None
         
         @dispatcher.add_method
-        def get_debits (address=None, asset=None, order_by=None, order_dir=None):
-            return util.get_debits(db,
-                address=address,
-                asset=asset,
-                order_by=order_by,
-                order_dir=order_dir)
-        
-        @dispatcher.add_method
-        def get_credits (address=None, asset=None, order_by=None, order_dir=None):
-            return util.get_credits(db,
-                address=address,
-                asset=asset,
-                order_by=order_by,
-                order_dir=order_dir)
-        
-        @dispatcher.add_method
-        def get_balances (address=None, asset=None, order_by=None, order_dir=None):
+        def get_balances (filters=[], order_by=None, order_dir=None):
+            if filters is None: filters=[]            
             return util.get_balances(db,
-                address=address,
-                asset=asset,
+                filters=filters,
                 order_by=order_by,
                 order_dir=order_dir)
 
         @dispatcher.add_method
-        def get_sends (source=None, destination=None, is_valid=None, order_by=None, order_dir=None, start_block=None, end_block=None):
-            return util.get_sends(db, 
-                source=source,
-                destination=destination,
-                validity='Valid' if bool(is_valid) else None,
-                order_by=order_by,
-                order_dir=order_dir,
-                start_block=start_block,
-                end_block=end_block)
-        
-        @dispatcher.add_method
-        def get_orders (address=None, is_valid=True, show_empty=True, show_expired=True, order_by=None, order_dir=None, start_block=None, end_block=None):
-            return util.get_orders(db,
-                address=address,
-                show_empty=show_empty,
-                show_expired=show_expired,
-                validity='Valid' if bool(is_valid) else None,
-                order_by=order_by,
-                order_dir=order_dir,
-                start_block=start_block,
-                end_block=end_block)
-        
-        @dispatcher.add_method
-        def get_order_matches (address=None, is_valid=True, is_mine=False, tx0_hash=None, tx1_hash=None, order_by=None, order_dir=None, start_block=None, end_block=None):
-            return util.get_order_matches(db,
-                is_mine=is_mine,
-                address=address,
-                tx0_hash=tx0_hash,
-                tx1_hash=tx1_hash,
-                validity='Valid' if bool(is_valid) else None,
-                order_by=order_by,
-                order_dir=order_dir,
-                start_block=start_block,
-                end_block=end_block)
-
-        @dispatcher.add_method
-        def get_btcpays(is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
-            return util.get_btcpays(db, 
-                validity='Valid' if bool(is_valid) else None,
-                order_by=order_by,
-                order_dir=order_dir,
-                start_block=start_block,
-                end_block=end_block)
-
-        @dispatcher.add_method
-        def get_issuances(asset=None, issuer=None, is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
-            return util.get_issuances(db,
-                asset=asset,
-                issuer=issuer,
-                validity='Valid' if bool(is_valid) else None,
-                order_by=order_by,
-                order_dir=order_dir,
-                start_block=start_block,
-                end_block=end_block)
-        
-        @dispatcher.add_method
-        def get_broadcasts(source=None, is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
-            return util.get_broadcasts(db,
-                source=source,
-                validity='Valid' if bool(is_valid) else None,
-                order_by=order_by,
-                order_dir=order_dir,
-                start_block=start_block,
-                end_block=end_block)
-        
-        @dispatcher.add_method
-        def get_bets(address=None, show_empty=False, is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+        def get_bets(filters=[], is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
             return util.get_bets(db,
-                address=address,
-                show_empty=show_empty,
+                filters=filters,
                 validity='Valid' if bool(is_valid) else None,
                 order_by=order_by,
                 order_dir=order_dir,
                 start_block=start_block,
                 end_block=end_block)
-        
+
         @dispatcher.add_method
-        def get_bet_matches(address=None, is_valid=True, tx0_hash=None, tx1_hash=None, order_by=None, order_dir=None, start_block=None, end_block=None):
+        def get_bet_matches(filters=[], is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
             return util.get_bet_matches(db,
-                address=address,
-                tx0_hash=tx0_hash,
-                tx1_hash=tx1_hash,
+                filters=filters,
                 validity='Valid' if bool(is_valid) else None,
                 order_by=order_by,
                 order_dir=order_dir,
                 start_block=start_block,
                 end_block=end_block)
-        
+
         @dispatcher.add_method
-        def get_dividends(address=None, asset=None, is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
-            return util.get_dividends(db,
-                address=address,
-                asset=asset,
+        def get_broadcasts(filters=[], is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
+            return util.get_broadcasts(db,
+                filters=filters,
                 validity='Valid' if bool(is_valid) else None,
                 order_by=order_by,
                 order_dir=order_dir,
                 start_block=start_block,
                 end_block=end_block)
-        
+
         @dispatcher.add_method
-        def get_burns(address=None, is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+        def get_btcpays(filters=[], is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
+            return util.get_btcpays(db, 
+                filters=filters,
+                validity='Valid' if bool(is_valid) else None,
+                order_by=order_by,
+                order_dir=order_dir,
+                start_block=start_block,
+                end_block=end_block)
+
+        @dispatcher.add_method
+        def get_burns(filters=[], is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
             return util.get_burns(db,
-                address=address,
+                filters=filters,
                 validity='Valid' if bool(is_valid) else None,
                 order_by=order_by,
                 order_dir=order_dir,
@@ -171,9 +101,10 @@ class reqthread ( threading.Thread ):
                 end_block=end_block)
 
         @dispatcher.add_method
-        def get_cancels(source=None, is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+        def get_cancels(filters=[], is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
             return util.get_cancels(db,
-                source=source,
+                filters=filters,
                 validity='Valid' if bool(is_valid) else None,
                 order_by=order_by,
                 order_dir=order_dir,
@@ -181,36 +112,80 @@ class reqthread ( threading.Thread ):
                 end_block=end_block)
 
         @dispatcher.add_method
-        def do_send(source, destination, quantity, asset, unsigned=False):
-            unsigned_tx_hex = send.create(db, source, destination, quantity, asset)
-            return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
+        def get_credits (filters=[], order_by=None, order_dir=None):
+            if filters is None: filters=[]            
+            return util.get_credits(db,
+                filters=filters,
+                order_by=order_by,
+                order_dir=order_dir)
+
+        @dispatcher.add_method
+        def get_debits (filters=[], order_by=None, order_dir=None):
+            if filters is None: filters=[]            
+            return util.get_debits(db,
+                filters=filters,
+                order_by=order_by,
+                order_dir=order_dir)
+
+        @dispatcher.add_method
+        def get_dividends(filters=[], is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
+            return util.get_dividends(db,
+                filters=filters,
+                validity='Valid' if bool(is_valid) else None,
+                order_by=order_by,
+                order_dir=order_dir,
+                start_block=start_block,
+                end_block=end_block)
+
+        @dispatcher.add_method
+        def get_issuances(filters=[], is_valid=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
+            return util.get_issuances(db,
+                filters=filters,
+                validity='Valid' if bool(is_valid) else None,
+                order_by=order_by,
+                order_dir=order_dir,
+                start_block=start_block,
+                end_block=end_block)
+
+        @dispatcher.add_method
+        def get_orders (filters=[], is_valid=True, show_expired=True, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
+            return util.get_orders(db,
+                filters=filters,
+                validity='Valid' if bool(is_valid) else None,
+                show_expired=show_expired,
+                order_by=order_by,
+                order_dir=order_dir,
+                start_block=start_block,
+                end_block=end_block)
         
         @dispatcher.add_method
-        def do_order(source, give_quantity, give_asset, get_quantity, get_asset, expiration, fee_required=0,
-                     fee_provided=config.MIN_FEE / config.UNIT, unsigned=False):
-            unsigned_tx_hex = order.create(db, source, give_asset,
-                                           give_quantity, get_asset,
-                                           get_quantity, expiration,
-                                           fee_required, fee_provided)
-            return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
-        
+        def get_order_matches (filters=[], is_valid=True, is_mine=False, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
+            return util.get_order_matches(db,
+                filters=filters,
+                validity='Valid' if bool(is_valid) else None,
+                is_mine=is_mine,
+                order_by=order_by,
+                order_dir=order_dir,
+                start_block=start_block,
+                end_block=end_block)
+
         @dispatcher.add_method
-        def do_btcpay(order_match_id, unsigned=False):
-            unsigned_tx_hex = btcpay.create(db, order_match_id)
-            return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
-        
-        @dispatcher.add_method
-        def do_issuance(source, quantity, asset, divisible, transfer_destination=None, unsigned=False):
-            unsigned_tx_hex = issuance.create(db, source, transfer_destination,
-                                              asset, quantity, divisible)
-            return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
-        
-        @dispatcher.add_method
-        def do_broadcast(source, fee_multiplier, text, timestamp, value=0, unsigned=False):
-            unsigned_tx_hex = broadcast.create(db, source, timestamp,
-                                               value, fee_multiplier, text)
-            return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
-        
+        def get_sends (filters=[], is_valid=None, order_by=None, order_dir=None, start_block=None, end_block=None):
+            if filters is None: filters=[]            
+            return util.get_sends(db, 
+                filters=filters,
+                validity='Valid' if bool(is_valid) else None,
+                order_by=order_by,
+                order_dir=order_dir,
+                start_block=start_block,
+                end_block=end_block)
+
+        ######################
+        #WRITE/ACTION API
         @dispatcher.add_method
         def do_bet(source, feed_address, bet_type, deadline, wager, counterwager, target_value=0.0, leverage=5040, unsigned=False):
             bet_type_id = util.BET_TYPE_ID[bet_type]
@@ -219,13 +194,18 @@ class reqthread ( threading.Thread ):
                                          counterwager, target_value,
                                          leverage, expiration)
             return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
-        
+
         @dispatcher.add_method
-        def do_dividend(source, quantity_per_share, share_asset, unsigned=False):
-            unsigned_tx_hex = dividend.create(db, source, quantity_per_share,
-                                              share_asset)
+        def do_broadcast(source, fee_multiplier, text, timestamp, value=0, unsigned=False):
+            unsigned_tx_hex = broadcast.create(db, source, timestamp,
+                                               value, fee_multiplier, text)
             return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
-        
+
+        @dispatcher.add_method
+        def do_btcpay(order_match_id, unsigned=False):
+            unsigned_tx_hex = btcpay.create(db, order_match_id)
+            return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
+
         @dispatcher.add_method
         def do_burn(source, quantity, unsigned=False):
             unsigned_tx_hex = burn.create(db, source, quantity)
@@ -236,6 +216,34 @@ class reqthread ( threading.Thread ):
             unsigned_tx_hex = cancel.create(db, offer_hash)
             return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
 
+        @dispatcher.add_method
+        def do_dividend(source, quantity_per_share, share_asset, unsigned=False):
+            unsigned_tx_hex = dividend.create(db, source, quantity_per_share,
+                                              share_asset)
+            return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
+
+        @dispatcher.add_method
+        def do_issuance(source, quantity, asset, divisible, transfer_destination=None, unsigned=False):
+            unsigned_tx_hex = issuance.create(db, source, transfer_destination,
+                                              asset, quantity, divisible)
+            return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
+        
+        @dispatcher.add_method
+        def do_order(source, give_quantity, give_asset, get_quantity, get_asset, expiration, fee_required=0,
+                     fee_provided=config.MIN_FEE / config.UNIT, unsigned=False):
+            unsigned_tx_hex = order.create(db, source, give_asset,
+                                           give_quantity, get_asset,
+                                           get_quantity, expiration,
+                                           fee_required, fee_provided)
+            return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
+
+        @dispatcher.add_method
+        def do_send(source, destination, quantity, asset, unsigned=False):
+            unsigned_tx_hex = send.create(db, source, destination, quantity, asset)
+            return bitcoin.transmit(unsigned_tx_hex, unsigned=unsigned, ask=False)
+        
+
+        
         class Root(object):
             @cherrypy.expose
             @cherrypy.tools.json_out()
