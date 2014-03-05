c28ace2d014d4f6427e223135e329cbf2a35de8b
PhantomPhreak <phantomphreak@counterparty.co>
2014-03-04 21:49:23 -0500

    neg. balance check

--- lib/util.py
+++ lib/util.py
@@ -403,6 +403,7 @@ def debit (db, block_index, address, asset, amount, action=None, event=None):
 
     balance = round(old_balance - amount)
     balance = min(balance, config.MAX_INT)
+    assert balance >= 0
 
     bindings = {
         'amount': balance,
