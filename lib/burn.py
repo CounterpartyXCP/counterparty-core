f97bf5756e0c40aa8aafad767c2bc9941cdbf4ff
PhantomPhreak <phantomphreak@counterparty.co>
2014-07-29 15:42:11 -0400

    avoid floats

--- lib/burn.py
+++ lib/burn.py
@@ -74,7 +74,7 @@ def parse (db, tx, message=None):
 
         total_time = config.BURN_END - config.BURN_START
         partial_time = config.BURN_END - tx['block_index']
-        multiplier = 1000 * (1 + (.5 * Fraction(partial_time, total_time)))
+        multiplier = (1000 + (500 * Fraction(partial_time, total_time)))
         earned = round(burned * multiplier)
 
         # Credit source address with earned XCP.
