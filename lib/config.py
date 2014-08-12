82e2ef3840936718314091f79fae2ba15963a63a
Lars Kluge <l@larskluge.com>
2014-08-13 01:12:09 +0700

    Allow a maximum burn of 5,000,000 DOGE per address

--- lib/config.py
+++ lib/config.py
@@ -76,8 +76,8 @@ BLOCK_FIRST_MAINNET = BLOCK_FIRST_MAINNET_TESTCOIN
 BURN_START_MAINNET = BURN_START_MAINNET_TESTCOIN
 BURN_END_MAINNET = 2500000
 
-MAX_BURN_BY_ADDRESS = 1000000 * UNIT 	# 1M DOGE.
 BURN_MULTIPLIER = 1 					# from 1.5 to 1 XDP by DOGE.
+MAX_BURN_BY_ADDRESS = 5000000 * UNIT 	# 5M DOGE.
 
 # Protocol defaults
 # NOTE: If the DUST_SIZE constants are changed, they MUST also be changed in counterblockd/lib/config.py as well
