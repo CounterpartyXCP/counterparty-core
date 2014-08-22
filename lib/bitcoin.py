2215e000b3e6c90ccd9802ea2e69d871fc94e61b
Bob McElrath <bob_git@mcelrath.org>
2014-08-22 12:01:36 -0400

    Update wif prefixes for changes in pycoin interface

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -384,9 +384,10 @@ def sort_unspent_txouts(unspent, allow_unconfirmed_inputs):
     return unspent
 
 def private_key_to_public_key (private_key_wif):
-    # allowable_wif_prefixes = [
+    allowable_wif_prefixes = [b'x80', b'\xef'] # Bitcoin mainnet, testnet3
     try:
-        secret_exponent, compressed = wif_to_tuple_of_secret_exponent_compressed(private_key_wif, is_test=config.TESTNET)
+        secret_exponent, compressed = wif_to_tuple_of_secret_exponent_compressed(
+                private_key_wif, allowable_wif_prefixes)
     except EncodingError:
         raise exceptions.AltcoinSupportError('pycoin: unsupported WIF prefix')
     public_pair = public_pair_for_secret_exponent(generator_secp256k1, secret_exponent)
