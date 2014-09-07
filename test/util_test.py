62d266df887a6ad167990896931752c7cfe7a102
JahPowerBit <lightzarlboro@gmail.com>
2014-09-07 22:38:27 +0000

    rebase test suite step 1

--- test/util_test.py
+++ test/util_test.py
@@ -81,7 +81,7 @@ def insert_raw_transaction(raw_transaction, db):
     tx_index = block_index - config.BURN_START + 1
     tx = bitcoin.decode_raw_transaction(raw_transaction)
     tx_hash = hashlib.sha256(chr(tx_index).encode('utf-8')).hexdigest()
-    source, destination, btc_amount, fee, data = blocks.get_tx_info(tx, block_index)
+    source, destination, btc_amount, fee, data = yield from blocks.get_tx_info(tx, block_index)
     transaction = (tx_index, tx_hash, block_index, block_hash, block_time, source, destination, btc_amount, fee, data, True)
     cursor.execute('''INSERT INTO transactions VALUES (?,?,?,?,?,?,?,?,?,?,?)''', transaction)
     tx = list(cursor.execute('''SELECT * FROM transactions WHERE tx_index = ?''', (tx_index,)))[0]
@@ -126,7 +126,7 @@ def run_scenario(scenario):
         if transaction[0] != 'create_next_block':
             module = sys.modules['lib.{}'.format(transaction[0])]
             compose = getattr(module, 'compose')
-            unsigned_tx_hex = bitcoin.transaction(compose(db, *transaction[1]), **transaction[2])
+            unsigned_tx_hex = util.aio_run_synch(bitcoin.transaction(compose(db, *transaction[1]), **transaction[2]))
             insert_raw_transaction(unsigned_tx_hex, db)
         else:
             create_next_block(db, block_index=config.BURN_START + transaction[1], parse_block=True)
@@ -184,7 +184,7 @@ def vector_to_args(vector, functions=[]):
 
 def exec_tested_method(tx_name, method, tested_method, inputs, counterpartyd_db):
     if tx_name == 'bitcoin' and method == 'transaction':
-        return tested_method(inputs[0], **inputs[1])
+        return util.aio_run_synch(tested_method(inputs[0], **inputs[1]))
     else:
         return tested_method(counterpartyd_db, *inputs)
 
@@ -206,7 +206,7 @@ def check_ouputs(tx_name, method, inputs, outputs, error, records, counterpartyd
             if tx_name == 'order' and inputs[1]=='BTC':
                 print('give btc')
                 tx_params['fee_provided'] = DP['fee_provided']
-            unsigned_tx_hex = bitcoin.transaction(test_outputs, **tx_params)
+            unsigned_tx_hex = util.aio_run_synch(bitcoin.transaction(test_outputs, **tx_params))
             print(tx_name)
             print(unsigned_tx_hex)
 
