4e12188e9a9f7c3493fccb949f024d81b8b37834
JahPowerBit <lightzarlboro@gmail.com>
2014-09-26 00:36:36 +0000

    remove unused function

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,7 +1,6 @@
 #! /usr/bin/python3
 import util_test
 
-
 def test_scenario(scenario_name, transactions):
     new_dump, new_log = util_test.run_scenario(transactions)
     old_dump, old_log = util_test.load_scenario_ouput(scenario_name)
