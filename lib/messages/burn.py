ecd659fd4cdada604e92bc9dc1c62b9fb98c003f
Adam Krellenstein <adam@krellenstein.com>
2014-12-03 14:06:47 -0500

    PARTIAL

--- lib/messages/burn.py
+++ lib/messages/burn.py
@@ -67,54 +67,81 @@ def compose (db, source, quantity, overburn=False):
     return (source, [(destination, quantity)], None)
 
 def parse (db, tx, message=None):
-    burn_parse_cursor = db.cursor()
-    status = 'valid'
 
-    if status == 'valid':
-        problems = validate(db, tx['source'], tx['destination'], tx['btc_amount'], tx['block_index'], overburn=False)
-        if problems: status = 'invalid: ' + '; '.join(problems)
+    if config.TESTNET:
+        burn_parse_cursor = db.cursor()
+        status = 'valid'
 
-        if tx['btc_amount'] != None:
-            sent = tx['btc_amount']
+        if status == 'valid':
+            problems = validate(db, tx['source'], tx['destination'], tx['btc_amount'], tx['block_index'], overburn=False)
+            if problems: status = 'invalid: ' + '; '.join(problems)
+
+            if tx['btc_amount'] != None:
+                sent = tx['btc_amount']
+            else:
+                sent = 0
+
+        if status == 'valid':
+            # Calculate quantity of XCP earned. (Maximum 1 BTC in total, ever.)
+            cursor = db.cursor()
+            cursor.execute('''SELECT * FROM burns WHERE (status = ? AND source = ?)''', ('valid', tx['source']))
+            burns = cursor.fetchall()
+            already_burned = sum([burn['burned'] for burn in burns])
+            ONE = 1 * config.UNIT
+            max_burn = ONE - already_burned
+            if sent > max_burn: burned = max_burn   # Exceeded maximum burn; earn what you can.
+            else: burned = sent
+
+            total_time = config.BURN_END - config.BURN_START
+            partial_time = config.BURN_END - tx['block_index']
+            multiplier = (1000 + (500 * Fraction(partial_time, total_time)))
+            earned = round(burned * multiplier)
+
+            # Credit source address with earned XCP.
+            util.credit(db, tx['block_index'], tx['source'], config.XCP, earned, action='burn', event=tx['tx_hash'])
         else:
-            sent = 0
+            burned = 0
+            earned = 0
 
-    if status == 'valid':
-        # Calculate quantity of XCP earned. (Maximum 1 BTC in total, ever.)
-        cursor = db.cursor()
-        cursor.execute('''SELECT * FROM burns WHERE (status = ? AND source = ?)''', ('valid', tx['source']))
-        burns = cursor.fetchall()
-        already_burned = sum([burn['burned'] for burn in burns])
-        ONE = 1 * config.UNIT
-        max_burn = ONE - already_burned
-        if sent > max_burn: burned = max_burn   # Exceeded maximum burn; earn what you can.
-        else: burned = sent
+        tx_index = tx['tx_index']
+        tx_hash = tx['tx_hash']
+        block_index = tx['block_index']
+        source = tx['source']
 
-        total_time = config.BURN_END - config.BURN_START
-        partial_time = config.BURN_END - tx['block_index']
-        multiplier = (1000 + (500 * Fraction(partial_time, total_time)))
-        earned = round(burned * multiplier)
-
-        # Credit source address with earned XCP.
-        util.credit(db, tx['block_index'], tx['source'], config.XCP, earned, action='burn', event=tx['tx_hash'])
     else:
-        burned = 0
-        earned = 0
+        # Mainnet burns are hard‐coded.
+        with open('mainnet_burns.csv') as f:    # TODO: Put elsewhere?
+            # TODO: CSV
+
+        found = False
+        for line in csv:
+            if line['tx_hash'] == tx['tx_hash']:
+                found = True
+                break
+            else:
+                continue
+
+        if found:
+            util.credit(db, line['block_index'], line['source'], config.XCP, line['earned'], action='burn', event=line['tx_hash'])
+
+            tx_index = line['tx_index']
+            tx_hash = line['tx_hash']
+            block_index = line['block_index']
+            source = line['source']
 
     # Add parsed transaction to message-type–specific table.
     # TODO: store sent in table
     bindings = {
-        'tx_index': tx['tx_index'],
-        'tx_hash': tx['tx_hash'],
-        'block_index': tx['block_index'],
-        'source': tx['source'],
+        'tx_index': tx_index,
+        'tx_hash': tx_hash,
+        'block_index': block_index,
+        'source': source,
         'burned': burned,
         'earned': earned,
         'status': status,
     }
     sql='insert into burns values(:tx_index, :tx_hash, :block_index, :source, :burned, :earned, :status)'
     burn_parse_cursor.execute(sql, bindings)
-
 
     burn_parse_cursor.close()
 
