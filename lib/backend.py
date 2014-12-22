685e1f32afabbf48499241b0514535bb93f8a95f
Adam Krellenstein <adam@krellenstein.com>
2014-12-22 13:09:29 -0500

    Set Socket Default Timeout
    Solves #554

--- lib/backend.py
+++ lib/backend.py
@@ -6,6 +6,7 @@ import sys
 import json
 from decimal import Decimal as D
 from functools import lru_cache
+import socket
 
 import bitcoin as bitcoinlib
 import bitcoin.rpc as bitcoinlib_rpc
@@ -15,6 +16,7 @@ from lib import script
 from lib import config
 
 def get_proxy():
+    socket.setdefaulttimeout(10)
     if config.TESTNET:
         bitcoinlib.SelectParams('testnet')
     proxy = bitcoinlib_rpc.Proxy(service_url=config.BACKEND_RPC)
