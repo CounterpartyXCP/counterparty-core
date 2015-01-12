c43b9db4be12d0a332d244ec65729bf1d2a73164
JahPowerBit <lightzarlboro@gmail.com>
2015-01-12 00:33:47 +0000

    Include `apsw` and `serpent` installation in `setup.py`

--- setup.py
+++ setup.py
@@ -2,10 +2,47 @@
 
 from setuptools import setup, find_packages
 import os
+import zipfile
+import urllib
 
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as f:
-    README = f.read()
+def install_apsw():
+    try:
+        import apsw
+        return
+    except:
+        pass
+
+    print("downloading apsw.")
+    urllib.request.urlretrieve('https://github.com/rogerbinns/apsw/releases/download/3.8.5-r1/apsw-3.8.5-r1.zip', 'apsw-3.8.5-r1.zip')
+
+    print("extracting.")
+    with zipfile.ZipFile('apsw-3.8.5-r1.zip', 'r') as zip_file:
+        zip_file.extractall()
+
+    print("install apsw.")
+    os.system('cd apsw-3.8.5-r1 && python setup.py fetch --version=3.8.5 --all build --enable-all-extensions install')
+
+    print("clean files.")
+    os.system('rm -rf apsw-3.8.5-r1 && rm apsw-3.8.5-r1.zip')
+
+def install_serpent():
+    print("downloading serpent.")
+    urllib.request.urlretrieve('https://github.com/ethereum/serpent/archive/master.zip', 'serpent.zip')
+
+    print("extracting.")
+    with zipfile.ZipFile('serpent.zip', 'r') as zip_file:
+        zip_file.extractall()
+
+    print("install serpent.")
+    os.system('cd serpent-master && make && sudo make install')
+
+    print("clean files.")
+    os.system('rm -rf serpent-master && rm serpent.zip')
+
+def readme():
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, 'README.md')) as f:
+        return f.read()
 
 required_packages = [
     'appdirs==1.4.0',
@@ -30,20 +67,23 @@ required_repos = [
     'https://github.com/petertodd/python-bitcoinlib/archive/python-bitcoinlib-v0.3.0.tar.gz#egg=python-bitcoinlib-0.3.0'
 ]
 
-setup(name='counterparty-lib',
-      version='9.49.3',
-      description='Reference implementation of the Counterparty protocol.',
-      long_description=README,
-      classifiers=[
-          "Programming Language :: Python",
-      ],
-      url='https://github.com/CounterpartyXCP/counterpartyd',
-      keywords='counterparty, bitcoin',
-      packages=find_packages(),
-      zip_safe=False,
-      dependency_links=required_repos,
-      install_requires=required_packages,
-      include_package_data=True
-     )
+setup_options = {
+    'name': 'counterparty-lib',
+    'version': '9.49.3',
+    'description': 'Reference implementation of the Counterparty protocol.',
+    'long_description': readme(),
+    'classifiers': [
+      "Programming Language :: Python",
+    ],
+    'url': 'https://github.com/CounterpartyXCP/counterpartyd',
+    'keywords': 'counterparty, bitcoin',
+    'packages': find_packages(),
+    'zip_safe': False,
+    'dependency_links': required_repos,
+    'install_requires': required_packages,
+    'include_package_data': True
+}
 
-
+install_serpent()
+install_apsw()
+setup(**setup_options)
