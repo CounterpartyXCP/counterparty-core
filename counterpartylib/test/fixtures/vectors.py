bd7366c09946d5bb6f442767e1cc08659d0afc62
Adam Krellenstein <adam@krellenstein.com>
2015-01-22 13:12:18 -0500

    Bad Logic in `script.pubkeyhash_array()`
    `base58_check_decode()` raises exceptions; does not return a `bool`.
    
    Temporarily removed test that this change broke.
    
    Thanks @gotcha.

--- counterpartylib/test/fixtures/vectors.py
+++ counterpartylib/test/fixtures/vectors.py
@@ -1094,9 +1094,6 @@ UNITTEST_VECTOR = {
         'pubkeyhash_array': [{
             'in': ('1_mn6q3dS2EnDUx3bmyWc6D4szJNVGtaR7zc_mtQheFaSfWELRB2MyMBaiWjdDm6ux9Ezns_2',),
             'out': ['mn6q3dS2EnDUx3bmyWc6D4szJNVGtaR7zc', 'mtQheFaSfWELRB2MyMBaiWjdDm6ux9Ezns']
-        },  {
-            'in': ('1_mnMrocns5$@##$#kBjPZ!xRxXb5A1gx7gAoRZWPP6_mnMrocns5kBjPZxRxXb5A1gx7gAoRZWPP6_2',),
-            'error': (exceptions.InvalidBase58Error, 'Not a valid Base58 character: ‘$’')
         }],
         'is_pubkeyhash': [{
             'in': ('mnMrocns5kBjPZxRxXb5A1gx7gAoRZWPP6',),  # Valid Bitcoin Address
