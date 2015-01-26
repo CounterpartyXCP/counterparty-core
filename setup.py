0f4a1f1a67835b3f120935546dad5e4e42278612
JahPowerBit <lightzarlboro@gmail.com>
2015-01-26 15:16:51 +0000

    Improve setup.py
    - use `cmdclass` option and `run_command()` for all post-install tasks
    - add `download_bootstrap_db` command

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-
-from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+from setuptools import setup, find_packages, Command
 import os
-import zipfile
+import zipfile, tarfile
 import urllib.request
 import sys
 import shutil
-import logging
 
 CURRENT_VERSION = '9.49.4rc4'
 
@@ -18,52 +17,176 @@ installed. Also be mindful of updates to apsw. If you need a specific version
 of apsw or need to build against a specific version of SQLite then please
 follow these instuctions ->
 http://rogerbinns.github.io/apsw/build.html#recommended"""
-def install_apsw():
-    try:
-        import apsw
-        return
-    except:
+class install_apsw(Command):
+    description="Install APSW 3.8.7.3-r1 with the appropriate version of SQLite"
+    user_options=[]
+
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
         pass
 
-    print("downloading apsw.")
-    urllib.request.urlretrieve('https://github.com/rogerbinns/apsw/archive/3.8.7.3-r1.zip', 'apsw-3.8.7.3-r1.zip')
+    def run(self):
+        # In Windows APSW should be installed manually
+        if os.name == 'nt':
+            print('To complete the installation you have to install APSW: https://github.com/rogerbinns/apsw/releases');
+            return
 
-    print("extracting.")
-    with zipfile.ZipFile('apsw-3.8.7.3-r1.zip', 'r') as zip_file:
-        zip_file.extractall()
+        try:
+            import apsw
+            return
+        except:
+            pass
 
-    executable = sys.executable
-    if executable is None:
-        executable = "python"
+        print("downloading apsw.")
+        urllib.request.urlretrieve('https://github.com/rogerbinns/apsw/archive/3.8.7.3-r1.zip', 'apsw-3.8.7.3-r1.zip')
 
-    print("install apsw.")
-    install_command = ('cd apsw-3.8.7.3-r1 && {executable} '
-      'setup.py fetch --version=3.8.7.3 --all build '
-      '--enable-all-extensions install'.format(executable=executable)
-    )
-    os.system(install_command)
+        print("extracting.")
+        with zipfile.ZipFile('apsw-3.8.7.3-r1.zip', 'r') as zip_file:
+            zip_file.extractall()
 
-    print("clean files.")
-    shutil.rmtree('apsw-3.8.7.3-r1')
-    os.remove('apsw-3.8.7.3-r1.zip')
+        executable = sys.executable
+        if executable is None:
+            executable = "python"
 
-def install_serpent():
-    print("downloading serpent.")
-    urllib.request.urlretrieve('https://github.com/ethereum/serpent/archive/master.zip', 'serpent.zip')
+        print("install apsw.")
+        install_command = ('cd apsw-3.8.7.3-r1 && {executable} '
+          'setup.py fetch --version=3.8.7.3 --all build '
+          '--enable-all-extensions install'.format(executable=executable)
+        )
+        os.system(install_command)
 
-    print("extracting.")
-    with zipfile.ZipFile('serpent.zip', 'r') as zip_file:
-        zip_file.extractall()
+        print("clean files.")
+        shutil.rmtree('apsw-3.8.7.3-r1')
+        os.remove('apsw-3.8.7.3-r1.zip')
 
-    print("making serpent.")
-    os.system('cd serpent-master && make')
-    print("install serpent using sudo.")
-    print("hence it might request a password.")
-    os.system('cd serpent-master && sudo make install')
+class install_serpent(Command):
+    description="Install Ethereum Serpent"
+    user_options=[]
 
-    print("clean files.")
-    shutil.rmtree('serpent-master')
-    os.remove('serpent.zip')
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # In Windows Serpent should be installed manually
+        if os.name == 'nt':
+            print('To complete the installation you have to install Serpent: https://github.com/ethereum/serpent');
+            return
+
+        print("downloading serpent.")
+        urllib.request.urlretrieve('https://github.com/ethereum/serpent/archive/master.zip', 'serpent.zip')
+
+        print("extracting.")
+        with zipfile.ZipFile('serpent.zip', 'r') as zip_file:
+            zip_file.extractall()
+
+        print("making serpent.")
+        os.system('cd serpent-master && make')
+        print("install serpent using sudo.")
+        print("hence it might request a password.")
+        os.system('cd serpent-master && sudo make install')
+
+        print("clean files.")
+        shutil.rmtree('serpent-master')
+        os.remove('serpent.zip')
+
+class move_old_db(Command):
+    description="Move database from old to new default data directory"
+    user_options=[]
+
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import appdirs
+        from counterpartylib.lib import config
+
+        old_data_dir = appdirs.user_config_dir(appauthor='Counterparty', appname='counterpartyd', roaming=True)
+        old_database = os.path.join(old_data_dir, 'counterpartyd.9.db')
+        old_database_testnet = os.path.join(old_data_dir, 'counterpartyd.9.testnet.db')
+
+        new_data_dir = appdirs.user_data_dir(appauthor=config.XCP_NAME, appname=config.XCP_NAME.lower(), roaming=True)
+        new_database = os.path.join(new_data_dir, '{}.{}.db'.format(config.XCP_NAME.lower(), config.VERSION_MAJOR))
+        new_database_testnet = os.path.join(new_data_dir, '{}.{}.testnet.db'.format(config.XCP_NAME.lower(), config.VERSION_MAJOR))
+
+        # User have an old version of `counterpartyd`
+        if os.path.exists(old_data_dir):
+            # Move database
+            if not os.path.exists(new_data_dir):
+                os.makedirs(new_data_dir)
+                files_to_copy = {
+                    old_database: new_database,
+                    old_database_testnet: new_database_testnet
+                }
+                for src_file in files_to_copy:
+                    if os.path.exists(src_file):
+                        dest_file = files_to_copy[src_file]
+                        print('Copy {} to {}'.format(src_file, dest_file))
+                        shutil.copy(src_file, dest_file)
+
+class download_bootstrap_db(Command):    
+    description="Download bootstrap database"
+    user_options=[
+        ("yes", None, "Download bootstrap database"),
+        ("no", None, "Don't download bootstrap database"),
+        ("ask", None, "Ask to download bootstrap database")
+    ]
+    boolean_options=['yes', 'no', 'ask']
+
+    def initialize_options(self):
+        self.yes = False
+        self.no = False
+        self.ask = True
+
+    def finalize_options(self):
+        if self.yes or self.no:
+            self.ask = False
+
+    def download(self):
+        import appdirs
+        from counterpartylib.lib import config
+
+        data_dir = appdirs.user_data_dir(appauthor=config.XCP_NAME, appname=config.XCP_NAME.lower(), roaming=True)
+        database = os.path.join(data_dir, '{}.{}.db'.format(config.XCP_NAME.lower(), config.VERSION_MAJOR))
+        database_testnet = os.path.join(data_dir, '{}.{}.testnet.db'.format(config.XCP_NAME.lower(), config.VERSION_MAJOR))
+
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+
+        print("downloading mainnet database.")
+        urllib.request.urlretrieve('https://s3.amazonaws.com/counterparty-bootstrap/counterpartyd-db.latest.tar.gz', 'counterpartyd-db.latest.tar.gz')
+        print("extracting.")
+        with tarfile.open('counterpartyd-db.latest.tar.gz', 'r:gz') as tar_file:
+            tar_file.extractall()
+        print('Copy {} to {}'.format('counterpartyd.9.db', database))
+        shutil.copy('counterpartyd.9.db', database)
+
+        print("downloading testnet database.")
+        urllib.request.urlretrieve('https://s3.amazonaws.com/counterparty-bootstrap/counterpartyd-testnet-db.latest.tar.gz', 'counterpartyd-testnet-db.latest.tar.gz')
+        print("extracting.")
+        with tarfile.open('counterpartyd-testnet-db.latest.tar.gz', 'r:gz') as tar_file:
+            tar_file.extractall()
+        print('Copy {} to {}'.format('counterpartyd.9.testnet.db', database))
+        shutil.copy('counterpartyd.9.testnet.db', database)
+
+    def run(self):
+        if self.yes:
+            self.download()
+        elif self.ask:
+            if input('Do you want to download the Counterparty database from https://s3.amazonaws.com/counterparty-bootstrap/? (y/N) : ').lower() == 'y':
+                self.download()
+             
+class install(_install):
+    def run(self):
+        _install.do_egg_install(self)
+        self.run_command('install_apsw')
+        self.run_command('install_serpent')
+        self.run_command('download_bootstrap_db')
+        self.run_command('move_old_db')
 
 required_packages = [
     'appdirs==1.4.0',
@@ -103,47 +226,16 @@ setup_options = {
     'provides': ['counterpartylib'],
     'packages': find_packages(),
     'zip_safe': False,
-    'install_requires': required_packages,
     'setup_requires': ['appdirs==1.4.0'],
-    'include_package_data': True
+    'install_requires': required_packages,
+    'include_package_data': True,
+    'cmdclass': {
+        'install': install,
+        'download_bootstrap_db': download_bootstrap_db,
+        'move_old_db': move_old_db,
+        'install_apsw': install_apsw,
+        'install_serpent': install_serpent
+    }
 }
 
 setup(**setup_options)
-
-# In Windows APSW and Serpent should be installed manually
-if os.name != 'nt':
-    install_apsw()
-    install_serpent()
-else:
-    logging.warning('''Warning:
-To complete the installation you have to install:
-- APSW: https://github.com/rogerbinns/apsw/releases
-- Serpent: https://github.com/ethereum/serpent''')
-
-if sys.argv[1] == 'install':
-    # Move database from old to new default data directory
-    import appdirs
-    from counterpartylib.lib import config
-
-    old_datadir = appdirs.user_config_dir(appauthor='Counterparty', appname='counterpartyd', roaming=True)
-    old_database = os.path.join(old_datadir, 'counterpartyd.9.db')
-    old_database_testnet = os.path.join(old_datadir, 'counterpartyd.9.testnet.db')
-
-    new_datadir = appdirs.user_data_dir(appauthor=config.XCP_NAME, appname=config.XCP_NAME.lower(), roaming=True)
-    new_database = os.path.join(new_datadir, '{}.{}.db'.format(config.XCP_NAME.lower(), config.VERSION_MAJOR))
-    new_database_testnet = os.path.join(new_datadir, '{}.{}.testnet.db'.format(config.XCP_NAME.lower(), config.VERSION_MAJOR))
-
-    # User have an old version of `counterpartyd`
-    if os.path.exists(old_datadir):
-        # Move database
-        if not os.path.exists(new_datadir):
-            os.makedirs(new_datadir)
-            files_to_copy = {
-                old_database: new_database,
-                old_database_testnet: new_database_testnet
-            }
-            for src_file in files_to_copy:
-                if os.path.exists(src_file):
-                    dest_file = files_to_copy[src_file]
-                    logging.warning('Copy {} to {}'.format(src_file, dest_file))
-                    shutil.copy(src_file, dest_file)
