ac75454bed7e7a692561f5b1fd22f699a5660da2
Ouziel Slama <lightzarlboro@gmail.com>
2015-02-05 20:20:01 +0000

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 ## Command Line Interface Versions ##
-* v1.0.0 (2015/02/??)
+* v1.0.0 (2015/02/05)
 	* Initial Release
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ required_packages = [
     'colorlog>=2.4.0',
     'python-dateutil>=2.2',
     'requests>=2.3.0',
-    'counterparty-lib>=9.49.4rc7'
+    'counterparty-lib>=9.49.4'
 ]
 
 setup_options = {
