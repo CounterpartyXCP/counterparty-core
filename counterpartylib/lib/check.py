88ef45146cfd25777ebdbc77cde8f3358c6f2a80
ouziel-slama <lightzarlboro@gmail.com>
2015-04-16 14:06:19 +0200

    Check if `held[asset]` equal None.

--- counterpartylib/lib/check.py
+++ counterpartylib/lib/check.py
@@ -103,7 +103,7 @@ def asset_conservation(db):
     held = util.held(db)
     for asset in supplies.keys():
         asset_issued = supplies[asset]
-        asset_held = held[asset] if asset in held else 0
+        asset_held = held[asset] if asset in held and held[asset] != None else 0
         if asset_issued != asset_held:
             raise SanityError('{} {} issued ≠ {} {} held'.format(util.value_out(db, asset_issued, asset), asset, util.value_out(db, asset_held, asset), asset))
         logger.debug('{} has been conserved ({} {} both issued and held)'.format(asset, util.value_out(db, asset_issued, asset), asset))
