ba6d98e43e35acefac9d195acbe23b9e8898b7d9
robby-dermody <robbyd@counterparty.io>
2015-09-22 14:19:03 -0400

    removing commenting, change logging level for specific stmt

--- counterpartylib/lib/backend/addrindex.py
+++ counterpartylib/lib/backend/addrindex.py
@@ -231,13 +231,13 @@ def getrawtransaction_batch(txhash_list, verbose=False, _recursing=False):
     # get transactions from cache
     result = {}
     for tx_hash in txhash_list:
-        try: #TEMP error handling for better race condition diagnosis. REMOVE before pushing to master
+        try:
             if verbose:
                 result[tx_hash] = raw_transactions_cache[tx_hash]
             else:
                 result[tx_hash] = raw_transactions_cache[tx_hash]['hex']
-        except KeyError: #likely race condition
-            logger.warn("tx missing in rawtx cache: {} -- txhash_list size: {}, hash: {} / raw_transactions_cache size: {} / # rpc_batch calls: {} / txhash in noncached_txhashes: {} / txhash in txhash_list: {} -- list {}".format(
+        except KeyError: #shows up most likely due to finickyness with addrindex not always returning results that we need...
+            logger.debug("tx missing in rawtx cache: {} -- txhash_list size: {}, hash: {} / raw_transactions_cache size: {} / # rpc_batch calls: {} / txhash in noncached_txhashes: {} / txhash in txhash_list: {} -- list {}".format(
                 e, len(txhash_list), hashlib.md5(json.dumps(list(txhash_list)).encode()).hexdigest(), len(raw_transactions_cache), len(payload),
                 tx_hash in noncached_txhashes, tx_hash in txhash_list, list(txhash_list.difference(noncached_txhashes)) ))
             if not _recursing: #try again
