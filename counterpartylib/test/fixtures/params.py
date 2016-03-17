d12d0c498ae5b129a4ee0643a79a9875f343c10f
Ruben de Vries <ruben@rubensayshi.com>
2016-03-17 17:47:15 +0100

    change DP['default_block'] to DP['default_block_index'], add using DP['default_block_hash'].

--- counterpartylib/test/fixtures/params.py
+++ counterpartylib/test/fixtures/params.py
@@ -22,7 +22,8 @@ DEFAULT_PARAMS = {
     'burn_start': 310000,
     'burn_end': 4017708,
     'burn_quantity': int(.62 * UNIT),
-    'default_block': 310000 + 501   # Should be called `block_index`.
+    'default_block_index': 310000 + 501,
+    'default_block_hash': '2d62095b10a709084b1854b262de77cb9f4f7cd76ba569657df8803990ffbfc6c12bca3c18a44edae9498e1f0f054072e16eef32dfa5e3dd4be149009115b4b8'
 }
 DEFAULT_PARAMS['privkey'] = {addr: priv for (addr, priv, pub) in DEFAULT_PARAMS['addresses']}
 DEFAULT_PARAMS['pubkey'] = {addr: pub for (addr, priv, pub) in DEFAULT_PARAMS['addresses']}
