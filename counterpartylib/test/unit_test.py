bf1280a3347e623e7c0cb5b332e481605eec82f8
Ruben de Vries <ruben@rubensayshi.com>
2016-03-31 13:01:42 +0200

    add a way to have vectors for code paths before protocol changes.

--- counterpartylib/test/unit_test.py
+++ counterpartylib/test/unit_test.py
@@ -13,12 +13,12 @@ FIXTURE_DB = tempfile.gettempdir() + '/fixtures.unittest_fixture.db'
 
 
 @pytest.mark.usefixtures("api_server")
-def test_vector(tx_name, method, inputs, outputs, error, records, comment, server_db):
+def test_vector(tx_name, method, inputs, outputs, error, records, comment, mock_protocol_changes, server_db):
     """Test the outputs of unit test vector. If testing parse, execute the transaction data on test db."""
     if method == 'parse':
         util_test.insert_transaction(inputs[0], server_db)
         inputs += (inputs[0]['data'][4:],) # message arg
-    util_test.check_outputs(tx_name, method, inputs, outputs, error, records, comment, server_db)
+    util_test.check_outputs(tx_name, method, inputs, outputs, error, records, comment, mock_protocol_changes, server_db)
 
 # def test_gen(server_db, rawtransactions_db):
 #     """Test cases generator.
