425dbf258653843b29301706606cf9ac7f21f96d
F483 <fabian.barkhau@gmail.com>
2016-06-18 15:57:29 +0200

    added tests (broken)

---
+++ counterpartylib/test/mpc_set_deposit_test.py
@@ -0,0 +1,21 @@
+import json
+import pprint
+import tempfile
+import pytest
+from counterpartylib.test import conftest  # this is require near the top to do setup of the test suite
+from counterpartylib.test import util_test
+from counterpartylib.test.util_test import CURR_DIR
+from counterpartylib.test.fixtures.params import DP, ADDR
+from counterpartylib.lib import util
+
+
+FIXTURE_SQL_FILE = CURR_DIR + '/fixtures/scenarios/unittest_fixture.sql'
+FIXTURE_DB = tempfile.gettempdir() + '/fixtures.unittest_fixture.db'
+FIXTURES = json.load(open(CURR_DIR + "/fixtures/mpc_set_deposit.json"))
+
+
+@pytest.mark.usefixtures("server_db")
+@pytest.mark.usefixtures("api_server")
+def test_success(server_db):
+    result = util.api("mpc_set_deposit", FIXTURES["test_success"]["input"])
+    assert FIXTURES["test_success"]["output"] == result
