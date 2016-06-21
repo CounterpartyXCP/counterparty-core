cc3d63fb949161d37ee873d7d67ff9086c78d5f4
Ruben de Vries <ruben@rubensayshi.com>
2016-06-21 15:40:08 +0200

    ensure determinitic log

--- counterpartylib/lib/evm/processblock.py
+++ counterpartylib/lib/evm/processblock.py
@@ -123,7 +123,7 @@ def apply_transaction(db, block, tx):
         if tx.startgas < intrinsic_gas:
             raise InsufficientStartGas(rp('startgas', tx.startgas, intrinsic_gas))
 
-    log_tx.debug('TX NEW', tx_dict=tx.log_dict())
+    log_tx.debug('TX NEW', **tx.log_dict())
 
     # start transacting #################
 
