30abe7ef6ab1740254c4f37ccb970eefc6611f4c
Ruben de Vries <ruben@rubensayshi.com>
2016-06-21 15:55:35 +0200

    test enum

--- counterpartylib/test/evm/contracts_solidity_test.py
+++ counterpartylib/test/evm/contracts_solidity_test.py
@@ -2367,3 +2367,37 @@ contract testme2 {
     c2.main(20, True)
     gascost = b - s.block.get_balance(tester.a0)
     assert gascost == 114769
+
+
+def test_enum():
+    code = """
+contract test {
+    enum ActionChoices { GoLeft, GoRight, GoStraight, SitStill }
+    ActionChoices choice;
+    ActionChoices constant defaultChoice = ActionChoices.GoStraight;
+
+    function setGoStraight() {
+        choice = ActionChoices.GoStraight;
+    }
+
+    // Since enum types are not part of the ABI, the signature of "getChoice"
+    // will automatically be changed to "getChoice() returns (uint8)"
+    // for all matters external to Solidity. The integer type used is just
+    // large enough to hold all enum values, i.e. if you have more values,
+    // `uint16` will be used and so on.
+    function getChoice() returns (ActionChoices) {
+        return choice;
+    }
+
+    function getDefaultChoice() returns (uint) {
+        return uint(defaultChoice);
+    }
+}
+"""
+    s = state()
+    c = s.abi_contract(code, language="solidity")
+
+    assert c.getDefaultChoice() == 2
+    assert c.getChoice() == 0
+    c.setGoStraight()
+    assert c.getChoice() == 2
