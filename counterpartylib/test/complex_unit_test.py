ba6c433a14071dac2b0a83d0b46e485e4439d00a
Ruben de Vries <ruben@rubensayshi.com>
2016-07-06 15:38:51 +0200

    add mocked `backend.`getrawtransaction_batch` and `backend.searchrawtransactions`.

--- counterpartylib/test/complex_unit_test.py
+++ counterpartylib/test/complex_unit_test.py
@@ -46,6 +46,14 @@ def test_alice_bob(server_db):
     assert alice_balance2 == alice_balance - v
     assert bob_balance2 == bob_balance + v
 
+    # check API result
+    result = util.api("get_balances", {"filters": [
+        {'field': 'address', 'op': '==', 'value': alice},
+        {'field': 'asset', 'op': '==', 'value': 'XCP'},
+    ]})
+
+    assert result[0]['quantity'] == alice_balance2
+
     # -- do another TX
 
     # check alices UTXOs
