1378abb985a9f00f072b14959e56e0978e435d58
robby-dermody <robbyd@u20.org>
2017-05-01 15:52:36 +0000

    correct download URL

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup_options = {
         "Topic :: Office/Business :: Financial",
         "Topic :: System :: Distributed Computing"
     ],
-    'download_url': 'https://github.com/CounterpartyXCP/counterparty-cli/releases/tag/v' + APP_VERSION,
+    'download_url': 'https://github.com/CounterpartyXCP/counterparty-cli/releases/tag/' + APP_VERSION,
     'provides': ['counterpartycli'],
     'packages': find_packages(),
     'zip_safe': False,
