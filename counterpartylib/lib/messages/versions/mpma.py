491e1ee110bb4b7fddcbc08d1dde4734537220c7
chiguireitor <johnvillarzavatti@gmail.com>
2020-11-01 14:09:41 -0400

    Added activation flag for mpma subasset support

--- counterpartylib/lib/messages/versions/mpma.py
+++ counterpartylib/lib/messages/versions/mpma.py
@@ -94,8 +94,9 @@ def compose (db, source, asset_dest_quant_list, memo, memo_is_hex):
 
     out_balances = util.accumulate([(t[0], t[2]) for t in asset_dest_quant_list])
     for (asset, quantity) in out_balances:
-        # resolve subassets
-        asset = util.resolve_subasset_longname(db, asset)
+        if util.enabled('mpma_subasset_support'):
+            # resolve subassets
+            asset = util.resolve_subasset_longname(db, asset)
 
         if not isinstance(quantity, int):
             raise exceptions.ComposeError('quantities must be an int (in satoshis) for {}'.format(asset))
