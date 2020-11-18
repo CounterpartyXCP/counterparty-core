8752048e7573d7d2e219306591f78ef8177da1c9
cryptcoin-junkey <cryptcoin.junkey@gmail.com>
2020-11-18 04:00:26 +0000

    Wait until all DBs is downloaded.
    Signed-off-by: cryptcoin-junkey <cryptcoin.junkey@gmail.com>

--- docker/start.sh
+++ docker/start.sh
@@ -13,14 +13,28 @@ fi
 # where a volume is mounted over the share dir, like the fednode docker compose config does...)
 if [ ! -f /root/.local/share/monaparty/monaparty.db ]; then
     touch /root/.local/share/monaparty/monaparty.db # To reduce DB corruption by racing.
+    touch /root/.local/share/monaparty/downloading.lock
     echo "Downloading mainnet bootstrap DB..."
-    counterparty-server bootstrap --quiet
+    counterparty-server bootstrap --quiet && \
+        rm /root/.local/share/monaparty/downloading.lock
 fi
 if [ ! -f /root/.local/share/monaparty/monaparty.testnet.db ]; then
     touch /root/.local/share/monaparty/monaparty.testnet.db # To redure DB corruption by racing.
+    touch /root/.local/share/monaparty/downloading.testnet.lock
     echo "Downloading testnet bootstrap DB..."
-    counterparty-server --testnet bootstrap --quiet
+    counterparty-server --testnet bootstrap --quiet && \
+        rm /root/.local/share/monaparty/downloading.testnet.lock
 fi
+
+while [ -f /root/.local/share/monaparty/downloading.lock ]; do
+    echo "Waiting mainnet bootstrap DB downloaded..."
+    sleep 3
+done
+while [ -f /root/.local/share/monaparty/downloading.testnet.lock ]; do
+    echo "Waiting mainnet bootstrap DB downloaded..."
+    sleep 3
+done
+echo "All DBs are downloaded."
 
 # Kick off the server, defaulting to the "start" subcommand
 # Launch utilizing the SIGTERM/SIGINT propagation pattern from
