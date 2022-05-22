e47a284764f6fc50597692a90dd74dd53665cecc
Javier Varona <javier@contimita.com>
2022-05-21 22:39:36 -0400

    - Added a check for addresses sent in a mpma. Now counterparty returns an error when there is a wrong address format

--- counterpartylib/lib/address.py
+++ counterpartylib/lib/address.py
@@ -28,10 +28,13 @@ def pack(address):
             return b''.join([witver, witprog])
         except Exception as ne:
             try:
+                script.validate(address) #This will check if the address is valid
                 short_address_bytes = bitcoin.base58.decode(address)[:-4]
                 return short_address_bytes
             except bitcoin.base58.InvalidBase58Error as e:
                 raise e
+            except Exception as e:
+                raise Exception(('The address {} is not a valid bitcoin address ({})').format(address,'testnet' if config.TESTNET or config.REGTEST else 'mainnet'))
     else:
         try:
             short_address_bytes = bitcoin.base58.decode(address)[:-4]
