625142668b01b41fc82f0f4d4d48de229b6b618f
Javier Varona <javier@contimita.com>
2022-08-22 16:23:12 -0400

    - Fixing subassetformat

--- counterpartylib/lib/messages/issuance.py
+++ counterpartylib/lib/messages/issuance.py
@@ -399,7 +399,7 @@ def parse (db, tx, message, message_type_id):
             lock = None
             reset = None
             
-            asset_id, quantity, divisible, compacted_subasset_length = struct.unpack(SUBASSET_FORMAT, message[0:subasset_format_length])
+            asset_id, quantity, divisible, compacted_subasset_length = struct.unpack(subasset_format, message[0:subasset_format_length])
             
             if subasset_format_length <= 18:
                 asset_id, quantity, divisible, compacted_subasset_length = struct.unpack(subasset_format, message[0:subasset_format_length])
