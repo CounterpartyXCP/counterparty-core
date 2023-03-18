600ac290b75db03fffbebc61ef983df51b7b8cb8
Javier Varona <javier@contimita.com>
2023-03-17 21:19:17 -0400

    - Now, when creating a tx, if the final change is dust, it will be added to the miners fee instead of returning an error

--- counterpartylib/lib/transaction.py
+++ counterpartylib/lib/transaction.py
@@ -187,7 +187,7 @@ def construct_coin_selection(encoding, data_array, source, allow_unconfirmed_inp
 
 
     # pop inputs until we can pay for the fee
-    for coin in use_inputs:
+    for use_inputs_index, coin in use_inputs:
         logger.debug('New input: {}'.format(print_coin(coin)))
         inputs.append(coin)
         btc_in += round(coin['amount'] * config.UNIT)
@@ -206,8 +206,14 @@ def construct_coin_selection(encoding, data_array, source, allow_unconfirmed_inp
         btc_out = destination_btc_out + data_btc_out
         change_quantity = btc_in - (btc_out + final_fee)
         logger.debug('Size: {} Fee: {:.8f} Change quantity: {:.8f} BTC'.format(size, final_fee / config.UNIT, change_quantity / config.UNIT))
-        # If change is necessary, must not be a dust output.
-        if change_quantity == 0 or change_quantity >= regular_dust_size:
+        
+        #If after the sum of all the utxos the change is dust, then it will be added to the miners instead of returning an error
+        if (use_inputs_index == len(use_inputs)-1) and (change_quantity > 0) and (change_quantity < regular_dust_size):
+            sufficient_funds = True
+            final_fee = final_fee + change_quantity
+            change_quantity = 0
+        # If change is necessary, must not be a dust output.            
+        elif change_quantity == 0 or change_quantity >= regular_dust_size:
             sufficient_funds = True
             if len(inputs) >= desired_input_count:
                 break
