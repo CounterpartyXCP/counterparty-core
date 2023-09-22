7ea99e5b187c3c27888419e3ebb90d93a4810ea3
Javier Varona <javier@contimita.com>
2023-09-21 21:41:01 -0400

    - Fixed an bug when trying to create a dividend for an asset whose divisibility was changed with a reset

--- counterpartylib/lib/util.py
+++ counterpartylib/lib/util.py
@@ -505,6 +505,18 @@ def is_divisible(db, asset):
         if not issuances: raise exceptions.AssetError('No such asset: {}'.format(asset))
         return issuances[0]['divisible']
 
+def get_asset_issuer(db, asset):
+    """Check if the asset is divisible."""
+    if asset in (config.BTC, config.XCP):
+        return True
+    else:
+        cursor = db.cursor()
+        cursor.execute('''SELECT * FROM issuances \
+                          WHERE (status = ? AND asset = ?) ORDER BY tx_index DESC''', ('valid', asset))
+        issuances = cursor.fetchall()
+        if not issuances: raise exceptions.AssetError('No such asset: {}'.format(asset))
+        return issuances[0]['issuer']
+
 def value_input(quantity, asset, divisible):
     if asset == 'leverage':
         return round(quantity)
