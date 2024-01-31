43b1e8cbcd8d732e7865910900c15bea9c41a8bd
Ouziel Slama <ouziel@gmail.com>
2024-01-31 13:13:58 +0100

    share blocks equally between threads

--- counterpartylib/lib/prefetcher.py
+++ counterpartylib/lib/prefetcher.py
@@ -16,15 +16,13 @@ PREFETCHER_THREADS = []
 
 class Prefetcher(threading.Thread):
 
-    def __init__(self, thread_index, num_threads):
+    def __init__(self, thread_index, num_threads, thread_first_block):
         threading.Thread.__init__(self)
         self.stop_event = threading.Event()
         self.thread_index = thread_index
         self.num_threads = num_threads
-        if config.TESTNET:
-            self.block_first = config.BLOCK_FIRST_TESTNET
-        else:
-            self.block_first = config.BLOCK_FIRST
+        self.block_first = thread_first_block
+        self.fetch_block_index = thread_first_block
 
     def stop(self):
         self.stop_event.set()
@@ -40,29 +38,28 @@ class Prefetcher(threading.Thread):
                 logger.debug('Blockchain cache is full. Sleeping Prefetcher thread {}.'.format(self.thread_index))
                 time.sleep(10)
                 continue
-        
-            # TODO: hackish!
-            if BLOCKCHAIN_CACHE:
-                fetch_block_index = max(self.block_first-1, util.CURRENT_BLOCK_INDEX, max(BLOCKCHAIN_CACHE.keys())) + 1
-            else:
-                fetch_block_index = util.CURRENT_BLOCK_INDEX
-            BLOCKCHAIN_CACHE[fetch_block_index] = None
 
-            # logger.debug('Fetching block {} with Prefetcher thread {}.'.format(fetch_block_index, self.thread_index))
-            block_hash = backend.getblockhash(fetch_block_index)
+            BLOCKCHAIN_CACHE[self.fetch_block_index] = None
+
+            #logger.debug('Fetching block {} with Prefetcher thread {}.'.format(self.fetch_block_index, self.thread_index))
+            block_hash = backend.getblockhash(self.fetch_block_index)
             block = backend.getblock(block_hash)
             txhash_list, raw_transactions = backend.get_tx_list(block)
-            BLOCKCHAIN_CACHE[fetch_block_index] = {'block_hash': block_hash,
+            BLOCKCHAIN_CACHE[self.fetch_block_index] = {'block_hash': block_hash,
                                              'txhash_list': txhash_list,
                                              'raw_transactions': raw_transactions,
                                              'previous_block_hash': bitcoinlib.core.b2lx(block.hashPrevBlock),
                                              'block_time': block.nTime,
                                              'block_difficulty': block.difficulty}
 
+            self.fetch_block_index += self.num_threads
+
 def start_all(num_prefetcher_threads):
     # Block Prefetcher and Indexer
+    block_first = util.CURRENT_BLOCK_INDEX
     for thread_index in range(1, num_prefetcher_threads + 1):
-        prefetcher_thread = Prefetcher(thread_index, num_prefetcher_threads)
+        thread_first_block = block_first + thread_index - 1
+        prefetcher_thread = Prefetcher(thread_index, num_prefetcher_threads, thread_first_block)
         prefetcher_thread.daemon = True
         prefetcher_thread.start()
         PREFETCHER_THREADS.append(prefetcher_thread)
