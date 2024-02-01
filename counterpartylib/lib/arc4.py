6055b568bce1553335878423cf030f6fb774b200
Adam Krellenstein <adam@krellenstein.com>
2024-02-01 07:45:51 -0500

    TEMP Logging for Cache Misses; Faster ARC4 Library

--- counterpartylib/lib/arc4.py
+++ counterpartylib/lib/arc4.py
@@ -1,7 +1,7 @@
 import binascii
-from Crypto.Cipher import ARC4
+from arc4 import ARC4
 
 def init_arc4(seed):
     if isinstance(seed, str):
         seed = binascii.unhexlify(seed)
-    return ARC4.new(seed)
+    return ARC4(seed)
--- counterpartylib/lib/backend/__init__.py
+++ counterpartylib/lib/backend/__init__.py
@@ -69,12 +69,18 @@ def clear_pretx(txid):
 def getrawtransaction(tx_hash, verbose=False, skip_missing=False, block_index=None):
     if block_index and block_index in prefetcher.BLOCKCHAIN_CACHE:
         # TODO: it should be here!
-        logger.warning('Cache miss! Block index {} not found in Blockchain Cache.'.format(block_index)) 
+        logger.warning('Block index {} found in blockchain cache.'.format(block_index)) 
         if tx_hash in prefetcher.BLOCKCHAIN_CACHE[block_index]['raw_transactions']:
+            logger.warning('Cache hit! Transaction {} found in blockchain cache.'.format(tx_hash, block_index)) 
             return prefetcher.BLOCKCHAIN_CACHE[block_index]['raw_transactions'][tx_hash]
+        else:
+            print(prefetcher.BLOCKCHAIN_CACHE[block_index]['raw_transactions'].keys())
+            logger.warning('Transaction {} not found in blockchain cache.'.format(tx_hash))
+    else:
+        logger.warning('Cache miss! Block index {} not found in blockchain cache.'.format(block_index)) 
     if tx_hash in PRETX_CACHE:
         return PRETX_CACHE[tx_hash]
-    logger.warning('Cache miss! Transaction {} (in block {}) not found in Blockchain Cache.'.format(tx_hash, block_index)) 
+    logger.warning('Cache miss! Transaction {} not found in blockchain cache.'.format(tx_hash, block_index)) 
     return BACKEND().getrawtransaction(tx_hash, verbose=verbose, skip_missing=skip_missing)
 
 def getrawtransaction_batch(txhash_list, verbose=False, skip_missing=False):
