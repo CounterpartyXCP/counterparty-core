44d1d7563d6b9cbc4e2aef26f521db3b617565c3
Adam Krellenstein <adam@krellenstein.com>
2024-03-07 14:42:42 +0100

    Clean Up Bootstrap File Naming

--- counterparty-cli/counterpartycli/util.py
+++ counterparty-cli/counterpartycli/util.py
@@ -120,12 +120,12 @@ def bootstrap(testnet=False, overwrite=True, ask_confirmation=False, quiet=False
 
     # Set Constants.
     if testnet:
-        BOOTSTRAP_URL = 'https://bootstrap.counterparty.io/counterparty-db-testnet.latest.tar.gz'
-        TARBALL_PATH = os.path.join(tempfile.gettempdir(), 'counterpartyd-testnet-db.latest.tar.gz')
+        BOOTSTRAP_URL = 'https://bootstrap.counterparty.io/counterparty-testnet.latest.tar.gz'
+        TARBALL_PATH = os.path.join(tempfile.gettempdir(), 'counterparty-testnet.latest.tar.gz')
         DATABASE_PATH = os.path.join(data_dir, '{}.testnet.db'.format(config.APP_NAME))
     else:
-        BOOTSTRAP_URL = 'https://bootstrap.counterparty.io/counterparty-db.latest.tar.gz'
-        TARBALL_PATH = os.path.join(tempfile.gettempdir(), 'counterpartyd-db.latest.tar.gz')
+        BOOTSTRAP_URL = 'https://bootstrap.counterparty.io/counterparty.latest.tar.gz'
+        TARBALL_PATH = os.path.join(tempfile.gettempdir(), 'counterparty.latest.tar.gz')
         DATABASE_PATH = os.path.join(data_dir, '{}.db'.format(config.APP_NAME))
 
     # Prepare Directory.
