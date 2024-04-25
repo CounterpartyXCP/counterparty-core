85c9ff7a4c699be49dd92439d846592776e2b8e2
Ouziel Slama <ouziel@gmail.com>
2024-04-25 17:07:39 +0200

    fix tests

--- counterparty-core/counterpartycore/test/api_v2_test.py
+++ counterparty-core/counterpartycore/test/api_v2_test.py
@@ -31,7 +31,7 @@ def test_api_v2(request):
     dispenser_hash = "74db175c4669a3d3a59e3fcddce9e97fcd7d12c35b58ef31845a1b20a1739498"
     event = "CREDIT"
     event_index = 10
-    exclude_routes = ["compose", "unpack", "info", "mempool", "healthz", "backend"]
+    exclude_routes = ["compose", "unpack", "info", "mempool", "healthz", "bitcoin"]
     results = {}
     fixtures = {}
     with open(API_V2_FIXTURES, "r") as f:
