354ed32dbc04e83b636766fb48f4d8f716355504
Ouziel Slama <ouziel@gmail.com>
2024-05-10 17:08:20 +0200

    Fix potential memory leak with hash_by_sequence variable; cleaning

--- counterparty-core/counterpartycore/lib/follow.py
+++ counterparty-core/counterpartycore/lib/follow.py
@@ -22,93 +22,100 @@ class BlockchainWatcher:
         logger.debug("Initializing blockchain watcher...")
         self.db = db
         self.loop = asyncio.get_event_loop()
-        self.zmq_context = zmq.asyncio.Context()
+        self.connect_to_zmq()
+        self.mempool_block = []
+        self.mempool_block_hashes = []
+        self.raw_tx_cache = {}
+        self.hash_by_sequence = {}
+        self.last_block_check_time = 0
+        # clean mempool before starting
+        mempool.clean_mempool(self.db)
 
+    def connect_to_zmq(self):
+        self.zmq_context = zmq.asyncio.Context()
         self.zmq_sub_socket_sequence = self.zmq_context.socket(zmq.SUB)
         self.zmq_sub_socket_sequence.setsockopt(zmq.RCVHWM, 0)
         self.zmq_sub_socket_sequence.setsockopt_string(zmq.SUBSCRIBE, "rawtx")
         self.zmq_sub_socket_sequence.setsockopt_string(zmq.SUBSCRIBE, "hashtx")
         self.zmq_sub_socket_sequence.setsockopt_string(zmq.SUBSCRIBE, "sequence")
         self.zmq_sub_socket_sequence.connect("tcp://127.0.0.1:%i" % sequence_port)
-
         self.zmq_sub_socket_rawblock = self.zmq_context.socket(zmq.SUB)
         self.zmq_sub_socket_rawblock.setsockopt(zmq.RCVHWM, 0)
         self.zmq_sub_socket_rawblock.setsockopt_string(zmq.SUBSCRIBE, "rawblock")
         self.zmq_sub_socket_rawblock.connect("tcp://127.0.0.1:%i" % rawblock_port)
 
-        self.mempool_block = []
-        self.mempool_block_hashes = []
+    def receive_rawblock(self, body):
+        # parse blocks as they come in
+        decoded_block = deserialize.deserialize_block(body.hex(), use_txid=True)
+        # check if already parsed by block.catch_up()
+        existing_block = ledger.get_block_by_hash(self.db, decoded_block["block_hash"])
+        if existing_block is None:
+            blocks.parse_new_block(self.db, decoded_block)
+            mempool.clean_mempool(self.db)
 
-        self.raw_tx_cache = {}
-        self.hash_by_sequence = {}
+    def receive_hashtx(self, body, sequence):
+        self.hash_by_sequence[sequence] = body.hex()
 
-        self.last_block_check_time = 0
+    def receive_rawtx(self, body, sequence):
+        tx_hash = self.hash_by_sequence.get(sequence)
+        if tx_hash is None:
+            # when tx never seen in the mempool is included in a block
+            decoded_tx = deserialize.deserialize_tx(body.hex(), use_txid=True)
+            tx_hash = decoded_tx["tx_hash"]
+        if sequence in self.hash_by_sequence:
+            self.hash_by_sequence.pop(sequence)
+        # first time seeing this transaction means from mempool
+        if tx_hash not in self.raw_tx_cache:
+            self.raw_tx_cache[tx_hash] = body.hex()
+        # second time seeing this transaction means from valid block
+        # we can remove it from the cache
+        else:
+            self.raw_tx_cache.pop(tx_hash)
 
-        # clean mempool before starting
-        mempool.clean_mempool(self.db)
+    def receive_sequence(self, body):
+        hash = body[:32].hex()
+        label = chr(body[32])
+        # new transaction in mempool
+        if label == "A":
+            if hash not in self.mempool_block_hashes:
+                self.mempool_block_hashes.append(hash)
+                # get transaction from cache
+                raw_tx = self.raw_tx_cache[hash]
+                # add transaction to mempool block
+                logger.trace("Adding transaction to mempool block: %s" % hash)
+                logger.trace("Mempool block size: %s" % len(self.mempool_block))
+                self.mempool_block.append(raw_tx)
+                if len(self.mempool_block) == MEMPOOL_BLOCK_MAX_SIZE:
+                    # parse mempool block
+                    mempool.parse_mempool_transactions(self.db, self.mempool_block)
+                    # reset mempool block
+                    self.mempool_block = []
+                    self.mempool_block_hashes = []
+                    logger.debug("Waiting for new transactions from mempool or new block...")
+        # transaction removed from mempool for non-block inclusion reasons
+        elif label == "R":
+            mempool.clean_transaction_events(self.db, hash)
 
     def receive_message(self, topic, body, seq):
         sequence = "Unknown"
         if len(seq) == 4:
             sequence = str(struct.unpack("<I", seq)[-1])
         logger.trace("Received message: %s %s" % (topic, sequence))
-
         if topic == b"rawblock":
-            # parse blocks as they come in
-            decoded_block = deserialize.deserialize_block(body.hex(), use_txid=True)
-            # check if already parsed by block.catch_up()
-            existing_block = ledger.get_block_by_hash(self.db, decoded_block["block_hash"])
-            if existing_block is None:
-                blocks.parse_new_block(self.db, decoded_block)
-                mempool.clean_mempool(self.db)
-            print("self.hash_by_sequence:", len(self.hash_by_sequence))
-            print("self.raw_tx_cache:", len(self.raw_tx_cache))
+            self.receive_rawblock(body)
         elif topic == b"hashtx":
-            self.hash_by_sequence[sequence] = body.hex()
+            self.receive_hashtx(body, sequence)
         elif topic == b"rawtx":
-            tx_hash = self.hash_by_sequence.get(sequence)
-            if tx_hash is None:
-                decoded_tx = deserialize.deserialize_tx(body.hex(), use_txid=True)
-                tx_hash = decoded_tx["tx_hash"]
-            # first time seeing this transaction means from mempool
-            if tx_hash not in self.raw_tx_cache:
-                self.raw_tx_cache[tx_hash] = body.hex()
-            # second time seeing this transaction means from valid block
-            # we can remove it from the cache
-            else:
-                self.raw_tx_cache.pop(tx_hash)
-                if sequence in self.hash_by_sequence:
-                    self.hash_by_sequence.pop(sequence)
+            self.receive_rawtx(body, sequence)
         elif topic == b"sequence":
-            hash = body[:32].hex()
-            label = chr(body[32])
-            # new transaction in mempool
-            if label == "A":
-                if hash not in self.mempool_block_hashes:
-                    self.mempool_block_hashes.append(hash)
-                    # get transaction from cache
-                    raw_tx = self.raw_tx_cache[hash]
-                    # add transaction to mempool block
-                    logger.trace("Adding transaction to mempool block: %s" % hash)
-                    logger.trace("Mempool block size: %s" % len(self.mempool_block))
-                    self.mempool_block.append(raw_tx)
-                    if len(self.mempool_block) == MEMPOOL_BLOCK_MAX_SIZE:
-                        # parse mempool block
-                        mempool.parse_mempool_transactions(self.db, self.mempool_block)
-                        # reset mempool block
-                        self.mempool_block = []
-                        self.mempool_block_hashes = []
-                        logger.debug("Waiting for new transactions from mempool or new block...")
-            # transaction removed from mempool for non-block inclusion reasons
-            elif label == "R":
-                mempool.clean_transaction_events(self.db, hash)
+            self.receive_sequence(body)
 
     async def handle(self):
         try:
             # sequence topic
             topic, body, seq = await self.zmq_sub_socket_sequence.recv_multipart()
             self.receive_message(topic, body, seq)
-            # rawblock topic
+            # check every 10 seconds rawblock topic
             if time.time() - self.last_block_check_time > 10:
                 try:
                     topic, body, seq = await self.zmq_sub_socket_rawblock.recv_multipart(
@@ -120,7 +127,7 @@ class BlockchainWatcher:
                     pass
                 self.last_block_check_time = time.time()
         except Exception as e:
-            print(traceback.format_exc())
+            logger.error(traceback.format_exc())
             self.stop()
             raise e
         # schedule ourselves to receive the next message
@@ -128,7 +135,6 @@ class BlockchainWatcher:
 
     def start(self):
         logger.info("Starting blockchain watcher...")
-        # self.loop.add_signal_handler(signal.SIGINT, self.stop)
         self.loop.create_task(self.handle())
         self.loop.run_forever()
 
