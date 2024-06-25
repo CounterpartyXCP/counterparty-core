83eb9fa28e50205358452b6340a675c487ec5109
Ouziel Slama <ouziel@gmail.com>
2024-06-25 14:52:27 +0000

    Use py.allow_threads

--- counterparty-core/counterpartycore/lib/backend/rsfetcher.py
+++ counterparty-core/counterpartycore/lib/backend/rsfetcher.py
@@ -10,21 +10,23 @@ from counterpartycore.lib import config, util
 logger = logging.getLogger(config.LOGGER_NAME)
 
 WORKER_THREADS = 3
-PREFETCH_QUEUE_SIZE = 5
+PREFETCH_QUEUE_SIZE = 50
 
 
 class RSFetcher(metaclass=util.SingletonMeta):
-    def __init__(self, start_height=0, config=None):
-        if config is None:
-            config = {
+    def __init__(self, start_height=0, indexer_config=None):
+        if indexer_config is None:
+            self.config = {
                 "rpc_address": f"http://{config.BACKEND_CONNECT}:{config.BACKEND_PORT}",
                 "rpc_user": config.BACKEND_USER,
                 "rpc_password": config.BACKEND_PASSWORD,
                 "db_dir": config.FETCHER_DB,
                 "log_file": config.FETCHER_LOG,
                 "log_level": config.LOG_LEVEL_STRING,
+                "start_height": start_height,
             }
-        self.config = config | {"start_height": start_height}
+        else:
+            self.config = indexer_config | {"start_height": start_height}
         self.start_height = start_height
         self.next_height = start_height
         self.fetcher = None
