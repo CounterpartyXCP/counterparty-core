db46287072bc144e2c36baa9a154ffd89f3f6231
Ouziel Slama <ouziel@gmail.com>
2024-10-10 11:09:13 +0000

    fix is_server_ready()

--- counterparty-core/counterpartycore/lib/api/wsgi.py
+++ counterparty-core/counterpartycore/lib/api/wsgi.py
@@ -34,10 +34,12 @@ def is_server_ready():
             return True
     except RuntimeError:
         pass
-    if BACKEND_HEIGHT is None or CURRENT_BLOCK_TIME is None:
+    if BACKEND_HEIGHT is None:
         return False
     if util.CURRENT_BLOCK_INDEX in [BACKEND_HEIGHT, BACKEND_HEIGHT - 1]:
         return True
+    if CURRENT_BLOCK_TIME is None:
+        return False
     if time.time() - CURRENT_BLOCK_TIME < 60:
         return True
     return False
