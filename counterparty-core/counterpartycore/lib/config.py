bd9ee5ca9cb9d7c7cc364a927317ea9d988c4929
Ouziel Slama <ouziel@gmail.com>
2024-10-18 18:23:10 +0000

    Bump version

--- counterparty-core/counterpartycore/lib/config.py
+++ counterparty-core/counterpartycore/lib/config.py
@@ -5,7 +5,7 @@ UNIT = 100000000  # The same across assets.
 
 
 # Semantic Version
-__version__ = "10.5.0-alpha.1"  # for hatch
+__version__ = "10.5.0-alpha.2"  # for hatch
 VERSION_STRING = __version__
 version = VERSION_STRING.split("-")[0].split(".")
 VERSION_MAJOR = int(version[0])
