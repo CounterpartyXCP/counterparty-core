b2772cc030cbce6c67d1270425265653d0504c4d
Ouziel Slama <ouziel@gmail.com>
2024-11-10 12:00:48 +0000

    Add tests for gas system; refactor calculate_fee; fix get_transaction_count_for_last_period

--- counterparty-core/counterpartycore/lib/gas.py
+++ counterparty-core/counterpartycore/lib/gas.py
@@ -24,24 +24,23 @@ def initialise(db):
     )
 
 
-def get_transaction_count_for_last_period(db, transaction_id, block_index):
+def get_transaction_count_by_block(db, transaction_id, block_index):
     cursor = db.cursor()
-    count = cursor.execute(
-        f"""
-        SELECT count FROM transaction_count
-        WHERE transaction_id = ? AND block_index >= ? - {PERIOD_DURATION}
-        ORDER BY rowid DESC
-        LIMIT 1
-        """,  # noqa S608
-        (transaction_id, block_index),
-    ).fetchone()
+    sql = """
+    SELECT count FROM transaction_count
+    WHERE transaction_id = ? AND block_index == ?
+    ORDER BY rowid DESC
+    LIMIT 1
+    """  # noqa S608
+    binding = (transaction_id, block_index)
+    count = cursor.execute(sql, binding).fetchone()
     if count is None:
         return 0
     return count["count"]
 
 
 def increment_counter(db, transaction_id, block_index):
-    current_count = get_transaction_count_for_last_period(db, transaction_id, block_index)
+    current_count = get_transaction_count_by_block(db, transaction_id, block_index)
     new_count = current_count + 1
 
     bindings = {
@@ -52,10 +51,31 @@ def increment_counter(db, transaction_id, block_index):
     ledger.insert_record(db, "transaction_count", bindings, "INCREMENT_TRANSACTION_COUNT")
 
 
-def get_average_transactions(db, transaction_id, block_index):
+def get_transaction_count_for_last_period(db, transaction_id, block_index):
     if block_index < PERIOD_DURATION:
         return 0
+
+    cursor = db.cursor()
+    sql = """
+        SELECT SUM(count) as total_count FROM (
+            SELECT MAX(rowid) as rowid, block_index, count
+            FROM transaction_count
+            WHERE transaction_id = ?
+            AND block_index >= ? AND block_index < ?
+            GROUP BY block_index
+        )
+    """
+    bindings = (transaction_id, block_index - PERIOD_DURATION, block_index)
+    transaction_count = cursor.execute(sql, bindings).fetchone()
+    if transaction_count is None:
+        return 0
+    return transaction_count["total_count"] or 0
+
+
+def get_average_transactions(db, transaction_id, block_index):
     transaction_count = get_transaction_count_for_last_period(db, transaction_id, block_index)
+    if transaction_count == 0:
+        return 0
     # return average number of transactions for the last PERIOD_DURATION blocks
     return transaction_count // PERIOD_DURATION
 
@@ -74,6 +94,36 @@ def get_transaction_fee(db, transaction_id, block_index):
 
 
 def calculate_fee(x, a, b, base_fee, k):
+    """
+    Calculate the fee based on the number of transactions per block,
+    ensuring continuity at the transition point.
+
+    Parameters:
+    x (float): Number of transactions per period
+    a (float): Lower threshold (fee is zero below this)
+    b (float): Upper threshold (transition point to exponential growth)
+    base_fee (float): Base fee amount
+    k (float): Sigmoid steepness factor
+
+    Returns:
+    float: Calculated fee
+    """
+    m = 1.5  # Exponent
+    n = 100  # Exponential Scaling Factor
+
+    def sigmoid(t):
+        midpoint = (b - a) / 2 + a
+        return base_fee / (1 + math.exp(-k * (t - midpoint)))
+
+    if x <= a:
+        return 0
+    elif x <= b:
+        return sigmoid(x)
+    else:
+        return base_fee + (((x - b) ** m) / n)
+
+
+def calculate_fee_1(x, a, b, base_fee, k):
     """
     Calculate the fee based on the number of transactions per block,
     ensuring continuity at the transition point.
---
+++ counterparty-core/counterpartycore/test/gas_test.py
@@ -0,0 +1,75 @@
+import tempfile
+
+import pytest
+
+from counterpartycore.lib import gas
+from counterpartycore.test.fixtures.params import ADDR, DP  # noqa: F401
+from counterpartycore.test.util_test import CURR_DIR
+
+FIXTURE_SQL_FILE = CURR_DIR + "/fixtures/scenarios/unittest_fixture.sql"
+FIXTURE_DB = tempfile.gettempdir() + "/fixtures.unittest_fixture.db"
+
+TRANSACTION_ID = 101
+
+
+@pytest.mark.usefixtures("server_db")
+def test_gas_counter(server_db):
+    assert gas.get_average_transactions(server_db, 101, 802015) == 0
+
+    # two transactions by block during 2016 blocks
+    for i in range(2016):
+        gas.increment_counter(server_db, TRANSACTION_ID, 800000 + i)
+        gas.increment_counter(server_db, TRANSACTION_ID, 800000 + i)
+
+    assert gas.get_transaction_count_for_last_period(server_db, TRANSACTION_ID, 802016) == 2016 * 2
+
+    assert gas.get_average_transactions(server_db, TRANSACTION_ID, 802016) == 2
+
+    # 2.5 transactions by block during 2016 blocks
+    for i in range(1008):
+        gas.increment_counter(server_db, TRANSACTION_ID, 800000 + i)
+
+    assert gas.get_average_transactions(server_db, TRANSACTION_ID, 802016) == 2
+
+    # 3 transactions by block during 2016 blocks
+    for i in range(1008):
+        gas.increment_counter(server_db, TRANSACTION_ID, 800000 + i)
+
+    assert gas.get_average_transactions(server_db, TRANSACTION_ID, 802016) == 3
+
+
+@pytest.mark.usefixtures("server_db")
+def test_gas_counter_2(server_db):
+    assert gas.get_average_transactions(server_db, 101, 802015) == 0
+
+    # two transactions by block during 3000 blocks
+    for i in range(3000):
+        gas.increment_counter(server_db, TRANSACTION_ID, 800000 + i)
+        gas.increment_counter(server_db, TRANSACTION_ID, 800000 + i)
+
+    for i in range(802016, 803001):
+        assert gas.get_transaction_count_for_last_period(server_db, TRANSACTION_ID, i) == 2016 * 2
+        assert gas.get_average_transactions(server_db, TRANSACTION_ID, i) == 2
+
+    # then 4 transactions by block during 1032 blocks
+    for i in range(1008):
+        gas.increment_counter(server_db, TRANSACTION_ID, 803000 + i)
+        gas.increment_counter(server_db, TRANSACTION_ID, 803000 + i)
+        gas.increment_counter(server_db, TRANSACTION_ID, 803000 + i)
+        gas.increment_counter(server_db, TRANSACTION_ID, 803000 + i)
+
+    assert (
+        gas.get_transaction_count_for_last_period(server_db, TRANSACTION_ID, 804008)
+        == 1008 * 2 + 1008 * 4
+    )
+    assert gas.get_average_transactions(server_db, TRANSACTION_ID, 804008) == 3
+
+    # then no transaction during 1 block
+    assert gas.get_average_transactions(server_db, TRANSACTION_ID, 804009) == 2
+
+    # then no transaction during 1008 block
+    assert gas.get_average_transactions(server_db, TRANSACTION_ID, 804008 + 1008) == 2
+    assert gas.get_average_transactions(server_db, TRANSACTION_ID, 804008 + 1009) == 1
+    # 4 tx by block during 504 blocks, then 0 tx during 1512 blocks (1512 + 504 = 2016)
+    assert gas.get_average_transactions(server_db, TRANSACTION_ID, 804008 + 1512) == 1
+    assert gas.get_average_transactions(server_db, TRANSACTION_ID, 804008 + 1513) == 0
