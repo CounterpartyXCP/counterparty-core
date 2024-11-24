9fd409f3f11179614026f10ad00be102a43c9d23
Ouziel Slama <ouziel@gmail.com>
2024-11-24 10:19:17 +0000

    fix typo

--- counterparty-core/counterpartycore/test/regtest/scenarios/scenario_5_dispenser.py
+++ counterparty-core/counterpartycore/test/regtest/scenarios/scenario_5_dispenser.py
@@ -180,7 +180,7 @@ SCENARIO = [
             "dispenser": "$ADDRESS_1",
             "quantity": 4000,
         },
-        "expected_error": ["source and destination must be different"],
+        "expected_error": "source and destination must be different",
     },
     {
         "title": "Dispense 4: get 100 XCP",
