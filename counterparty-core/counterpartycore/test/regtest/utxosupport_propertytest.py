557ced9fe678fa391b7411b6e38ed72686793ba2
Ouziel Slama <ouziel@gmail.com>
2024-12-03 21:49:21 +0000

    Add property test for transactions chaining

--- counterparty-core/counterpartycore/test/regtest/utxosupport_propertytest.py
+++ counterparty-core/counterpartycore/test/regtest/utxosupport_propertytest.py
@@ -39,7 +39,7 @@ class UTXOSupportPropertyTest(PropertyTestNode):
             self.issue_assets,
             hypothesis.strategies.sampled_from(self.addresses),
             hypothesis.strategies.text(
-                alphabet="BCDEFGHIJKLMNOPQRSTUVWXYZ", min_size=5, max_size=10
+                alphabet="BCDEFGHIJKLMNOPQRSTUVWXYZ", min_size=5, max_size=8
             ),
             hypothesis.strategies.integers(min_value=10 * 1e8, max_value=1000 * 1e8),
         )
@@ -71,6 +71,17 @@ class UTXOSupportPropertyTest(PropertyTestNode):
             hypothesis.strategies.sampled_from(self.addresses),
             hypothesis.strategies.sampled_from(MESSAGE_IDS),
             hypothesis.strategies.binary(min_size=20, max_size=30),
+        )
+
+        # issue, attach, move and detach assets chaining
+        self.test_with_given_data(
+            self.issue_attach_move_detach_send,
+            hypothesis.strategies.sampled_from(self.addresses),
+            hypothesis.strategies.text(
+                alphabet="BCDEFGHIJKLMNOPQRSTUVWXYZ", min_size=9, max_size=11
+            ),
+            hypothesis.strategies.integers(min_value=10 * 1e8, max_value=1000 * 1e8),
+            hypothesis.strategies.sampled_from(self.addresses),
         )
 
     @settings(deadline=None)
@@ -157,6 +168,81 @@ class UTXOSupportPropertyTest(PropertyTestNode):
                 upserts.append([f"{tx_hash}:1", asset, quantity, utxo_address])
         for upsert in upserts:
             self.upsert_balance(*upsert)
+
+    @settings(deadline=None, max_examples=20)
+    def issue_attach_move_detach_send(self, source, asset_name, quantity, destination):
+        # don't issue the same asset twice
+        # and don't issue twice from the same source
+        for balance in self.balances:
+            if balance[1] == asset_name or (balance[0] == source and len(balance[1]) > 8):
+                return
+        tx_hash = self.send_transaction(
+            source,
+            "issuance",
+            {
+                "asset": asset_name,
+                "quantity": quantity,
+                "exact_fee": 0,
+            },
+        )
+        tx_hash = self.send_transaction(
+            source,
+            "attach",
+            {
+                "asset": asset_name,
+                "quantity": quantity,
+                "exact_fee": 0,
+                "validate": False,
+                "inputs_set": f"{tx_hash}:1",
+            },
+        )
+        tx_hash = self.send_transaction(
+            f"{tx_hash}:1",
+            "movetoutxo",
+            {
+                "destination": source,
+                "exact_fee": 0,
+                "validate": False,
+                "inputs_set": f"{tx_hash}:1",
+            },
+        )
+        tx_hash = self.send_transaction(
+            f"{tx_hash}:0",
+            "movetoutxo",
+            {
+                "destination": source,
+                "exact_fee": 0,
+                "validate": False,
+                "inputs_set": f"{tx_hash}:0",
+            },
+        )
+        tx_hash = self.send_transaction(
+            f"{tx_hash}:0",
+            "detach",
+            {
+                "exact_fee": 0,
+                "validate": False,
+                "inputs_set": f"{tx_hash}:0",
+            },
+        )
+        if destination != source:
+            print(f"source: {source}, destination: {destination}")
+            tx_hash = self.send_transaction(
+                source,
+                "send",
+                {
+                    "destination": destination,
+                    "asset": asset_name,
+                    "quantity": int(10 * 1e8),
+                    "exact_fee": 0,
+                    "validate": False,
+                    "inputs_set": f"{tx_hash}:1",
+                },
+            )
+            self.upsert_balance(source, asset_name, quantity - 10 * 1e8)
+            self.upsert_balance(destination, asset_name, 10 * 1e8)
+        else:
+            self.upsert_balance(source, asset_name, quantity)
 
 
 if __name__ == "__main__":
