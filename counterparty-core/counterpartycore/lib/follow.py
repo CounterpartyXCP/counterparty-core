256096a1f5e060468f51fb8aaa61a5fc0d31cc3e
Ouziel Slama <ouziel@gmail.com>
2024-12-16 10:48:26 +0000

    clean debug

--- counterparty-core/counterpartycore/lib/follow.py
+++ counterparty-core/counterpartycore/lib/follow.py
@@ -272,9 +272,6 @@ class BlockchainWatcher:
                 break  # Exit the loop if the task is cancelled
             except Exception as e:
                 logger.error("Error in handle loop: %s", e)
-                import traceback
-
-                print(traceback.format_exc())  # for debugging
                 capture_exception(e)
                 break  # Optionally break the loop on other exceptions
 
