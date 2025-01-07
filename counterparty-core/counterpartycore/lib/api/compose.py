779453fe25b6d0db5d12f84ae4e69bda82a4b2d1
Ouziel Slama <ouziel@gmail.com>
2025-01-07 08:14:22 +0000

    fix regtest; test block deserialization

--- counterparty-core/counterpartycore/lib/api/compose.py
+++ counterparty-core/counterpartycore/lib/api/compose.py
@@ -566,7 +566,8 @@ def info(db, rawtransaction: str, block_index: int = None):
     except exceptions.BitcoindRPCError:
         source, destination, btc_amount, fee, data = None, None, None, None, None
 
-    del decoded_tx["__data__"]
+    if "__data__" in decoded_tx:
+        del decoded_tx["__data__"]
     result = {
         "source": source,
         "destination": destination if destination else None,
