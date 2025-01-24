93aab7902789a7553fda0e1bd906353cb3130efb
Ouziel Slama <ouziel@gmail.com>
2025-01-24 15:45:58 +0000

    finish order.py migration

--- counterparty-core/counterpartycore/pytest/units/messages/order_test.py
+++ counterparty-core/counterpartycore/pytest/units/messages/order_test.py
@@ -1,7 +1,6 @@
 import pytest
 from counterpartycore.lib import exceptions
 from counterpartycore.lib.messages import order
-from counterpartycore.lib.utils import helpers
 
 
 def test_validate(ledger_db, defaults, current_block_index):
@@ -406,6 +405,54 @@ def test_compose(ledger_db, defaults, current_block_index):
         b"\n\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05\xf5\xe1\x00\x00\x06\xca\xd8\xdc\x7f\x0bf\x00\x00\x00\x00\x00\x00\x01\xf4\x07\xd0\x00\x00\x00\x00\x00\x00\x00\x00",
     )
 
+    assert order.compose(
+        ledger_db,
+        defaults["p2ms_addresses"][0],
+        "BTC",
+        defaults["quantity"],
+        "XCP",
+        defaults["quantity"],
+        2000,
+        0,
+        current_block_index,
+    ) == (
+        defaults["p2ms_addresses"][0],
+        [],
+        b"\n\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x05\xf5\xe1\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05\xf5\xe1\x00\x07\xd0\x00\x00\x00\x00\x00\x00\x00\x00",
+    )
+
+    assert order.compose(
+        ledger_db,
+        defaults["p2ms_addresses"][0],
+        "XCP",
+        100000000,
+        "BTC",
+        500000,
+        2000,
+        0,
+        current_block_index,
+    ) == (
+        defaults["p2ms_addresses"][0],
+        [],
+        b"\n\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05\xf5\xe1\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x07\xa1 \x07\xd0\x00\x00\x00\x00\x00\x00\x00\x00",
+    )
+
+    assert order.compose(
+        ledger_db,
+        defaults["p2ms_addresses"][0],
+        "XCP",
+        100000000,
+        "NODIVISIBLE",
+        500,
+        2000,
+        0,
+        current_block_index,
+    ) == (
+        defaults["p2ms_addresses"][0],
+        [],
+        b"\n\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05\xf5\xe1\x00\x00\x06\xca\xd8\xdc\x7f\x0bf\x00\x00\x00\x00\x00\x00\x01\xf4\x07\xd0\x00\x00\x00\x00\x00\x00\x00\x00",
+    )
+
 
 def test_parse_order(ledger_db, blockchain_mock, defaults, test_helpers, current_block_index):
     tx = blockchain_mock.dummy_tx(ledger_db, defaults["addresses"][1], fee=10000)
@@ -757,23 +804,6 @@ def test_parse_order_invalid_data(ledger_db, blockchain_mock, defaults, test_hel
     message = b"\x00\x00\x00\x00\x00 foo\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xe1\x00\x07\xd0\x00\x00\x00\x00\x00\x00\x00\x00"
     order.parse(ledger_db, tx, message)
 
-    print(
-        helpers.to_json(
-            ledger_db.execute(
-                "SELECT * FROM orders WHERE tx_hash = ?", (tx["tx_hash"],)
-            ).fetchone(),
-            indent=4,
-        )
-    )
-    print(
-        helpers.to_json(
-            ledger_db.execute(
-                "SELECT * FROM order_matches WHERE id like ?", (f"%{tx['tx_hash']}%",)
-            ).fetchone(),
-            indent=4,
-        )
-    )
-
     test_helpers.check_records(
         ledger_db,
         [
@@ -803,27 +833,12 @@ def test_parse_order_invalid_data(ledger_db, blockchain_mock, defaults, test_hel
     )
 
 
-def test_parse_order_no_divisible(ledger_db, blockchain_mock, defaults, test_helpers):
+def test_parse_order_no_divisible(
+    ledger_db, blockchain_mock, defaults, test_helpers, current_block_index
+):
     tx = blockchain_mock.dummy_tx(ledger_db, defaults["addresses"][1], fee=10000)
     message = b"\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05\xf5\xe1\x00\x00\x06\xca\xd8\xdc\x7f\x0bf\x00\x00\x00\x00\x00\x00\x01\xf4\x07\xd0\x00\x00\x00\x00\x00\x00\x00\x00"
     order.parse(ledger_db, tx, message)
-
-    print(
-        helpers.to_json(
-            ledger_db.execute(
-                "SELECT * FROM orders WHERE tx_hash = ?", (tx["tx_hash"],)
-            ).fetchone(),
-            indent=4,
-        )
-    )
-    print(
-        helpers.to_json(
-            ledger_db.execute(
-                "SELECT * FROM order_matches WHERE id like ?", (f"%{tx['tx_hash']}%",)
-            ).fetchone(),
-            indent=4,
-        )
-    )
 
     test_helpers.check_records(
         ledger_db,
@@ -849,6 +864,229 @@ def test_parse_order_no_divisible(ledger_db, blockchain_mock, defaults, test_hel
                     "fee_provided_remaining": 10000,
                     "status": "open",
                 },
-            }
+            },
+            {
+                "table": "debits",
+                "values": {
+                    "action": "open order",
+                    "address": defaults["addresses"][1],
+                    "asset": "XCP",
+                    "block_index": current_block_index,
+                    "event": tx["tx_hash"],
+                    "quantity": 100000000,
+                },
+            },
         ],
     )
+
+
+def test_parse_order_multisig(
+    ledger_db, blockchain_mock, defaults, test_helpers, current_block_index
+):
+    tx = blockchain_mock.dummy_tx(ledger_db, defaults["p2ms_addresses"][0], fee=10000)
+    message = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x05\xf5\xe1\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05\xf5\xe1\x00\x07\xd0\x00\x00\x00\x00\x00\x00\x00\x00"
+    order.parse(ledger_db, tx, message)
+
+    test_helpers.check_records(
+        ledger_db,
+        [
+            {
+                "table": "orders",
+                "values": {
+                    "tx_index": tx["tx_index"],
+                    "tx_hash": tx["tx_hash"],
+                    "block_index": current_block_index,
+                    "source": defaults["p2ms_addresses"][0],
+                    "give_asset": "BTC",
+                    "give_quantity": 100000000,
+                    "give_remaining": 99200000,
+                    "get_asset": "XCP",
+                    "get_quantity": 100000000,
+                    "get_remaining": 0,
+                    "expiration": 2000,
+                    "expire_index": tx["block_index"] + 2000,
+                    "fee_required": 0,
+                    "fee_required_remaining": 0,
+                    "fee_provided": 10000,
+                    "fee_provided_remaining": 2800,
+                    "status": "open",
+                },
+            },
+            {
+                "table": "order_matches",
+                "values": {
+                    "tx0_address": defaults["addresses"][0],
+                    "tx1_index": tx["tx_index"],
+                    "tx1_hash": tx["tx_hash"],
+                    "tx1_address": defaults["p2ms_addresses"][0],
+                    "forward_asset": "XCP",
+                    "forward_quantity": 100000000,
+                    "backward_asset": "BTC",
+                    "backward_quantity": 800000,
+                    "tx1_block_index": tx["block_index"],
+                    "block_index": tx["block_index"],
+                    "tx0_expiration": 2000,
+                    "tx1_expiration": 2000,
+                    "match_expire_index": tx["block_index"] + 20,
+                    "fee_paid": 7200,
+                    "status": "pending",
+                },
+            },
+        ],
+    )
+
+
+def test_parse_order_multisig2(
+    ledger_db, blockchain_mock, defaults, test_helpers, current_block_index
+):
+    tx = blockchain_mock.dummy_tx(ledger_db, defaults["p2ms_addresses"][0], fee=10000)
+    message = b"\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05\xf5\xe1\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x07\xa1 \x07\xd0\x00\x00\x00\x00\x00\x00\x00\x00"
+    order.parse(ledger_db, tx, message)
+
+    test_helpers.check_records(
+        ledger_db,
+        [
+            {
+                "table": "orders",
+                "values": {
+                    "tx_index": tx["tx_index"],
+                    "tx_hash": tx["tx_hash"],
+                    "block_index": current_block_index,
+                    "source": defaults["p2ms_addresses"][0],
+                    "give_asset": "XCP",
+                    "give_quantity": 100000000,
+                    "give_remaining": 0,
+                    "get_asset": "BTC",
+                    "get_quantity": 500000,
+                    "get_remaining": -300000,
+                    "expiration": 2000,
+                    "expire_index": tx["block_index"] + 2000,
+                    "fee_required": 0,
+                    "fee_required_remaining": 0,
+                    "fee_provided": 10000,
+                    "fee_provided_remaining": 10000,
+                    "status": "open",
+                },
+            },
+            {
+                "table": "order_matches",
+                "values": {
+                    "tx0_address": defaults["addresses"][1],
+                    "tx1_index": tx["tx_index"],
+                    "tx1_hash": tx["tx_hash"],
+                    "tx1_address": defaults["p2ms_addresses"][0],
+                    "forward_asset": "BTC",
+                    "forward_quantity": 800000,
+                    "backward_asset": "XCP",
+                    "backward_quantity": 100000000,
+                    "tx1_block_index": tx["block_index"],
+                    "block_index": tx["block_index"],
+                    "tx0_expiration": 2000,
+                    "tx1_expiration": 2000,
+                    "match_expire_index": tx["block_index"] + 20,
+                    "fee_paid": 0,
+                    "status": "pending",
+                },
+            },
+        ],
+    )
+
+
+def test_parse_order_multisig3(
+    ledger_db, blockchain_mock, defaults, test_helpers, current_block_index
+):
+    tx = blockchain_mock.dummy_tx(ledger_db, defaults["p2ms_addresses"][0], fee=10000)
+    message = b"\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05\xf5\xe1\x00\x00\x06\xca\xd8\xdc\x7f\x0bf\x00\x00\x00\x00\x00\x00\x01\xf4\x07\xd0\x00\x00\x00\x00\x00\x00\x00\x00"
+    order.parse(ledger_db, tx, message)
+
+    test_helpers.check_records(
+        ledger_db,
+        [
+            {
+                "table": "orders",
+                "values": {
+                    "tx_index": tx["tx_index"],
+                    "tx_hash": tx["tx_hash"],
+                    "block_index": tx["block_index"],
+                    "source": defaults["p2ms_addresses"][0],
+                    "give_asset": "XCP",
+                    "give_quantity": 100000000,
+                    "give_remaining": 100000000,
+                    "get_asset": "NODIVISIBLE",
+                    "get_quantity": 500,
+                    "get_remaining": 500,
+                    "expiration": 2000,
+                    "expire_index": tx["block_index"] + 2000,
+                    "fee_required": 0,
+                    "fee_required_remaining": 0,
+                    "fee_provided": 10000,
+                    "fee_provided_remaining": 10000,
+                    "status": "open",
+                },
+            },
+            {
+                "table": "debits",
+                "values": {
+                    "action": "open order",
+                    "address": defaults["p2ms_addresses"][0],
+                    "asset": "XCP",
+                    "block_index": current_block_index,
+                    "event": tx["tx_hash"],
+                    "quantity": 100000000,
+                },
+            },
+        ],
+    )
+
+
+def test_parse_order_maxi(ledger_db, blockchain_mock, defaults, test_helpers, current_block_index):
+    tx = blockchain_mock.dummy_tx(ledger_db, defaults["addresses"][0], fee=10000)
+    message = b"\x00\x00\x00\x00\x00\x03:>\x7f\xff\xff\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05\xf5\xe1\x00\x00\n\x00\x00\x00\x00\x00\r\xbb\xa0"
+    order.parse(ledger_db, tx, message)
+
+    test_helpers.check_records(
+        ledger_db,
+        [
+            {
+                "table": "orders",
+                "values": {
+                    "block_index": tx["block_index"],
+                    "expiration": 10,
+                    "expire_index": tx["block_index"] + 10,
+                    "fee_provided": 10000,
+                    "fee_provided_remaining": 10000,
+                    "fee_required": 900000,
+                    "fee_required_remaining": 900000,
+                    "get_asset": "XCP",
+                    "get_quantity": 100000000,
+                    "get_remaining": 100000000,
+                    "give_asset": "MAXI",
+                    "give_quantity": 9223372036854775807,
+                    "give_remaining": 9223372036854775807,
+                    "source": defaults["addresses"][0],
+                    "status": "open",
+                    "tx_hash": tx["tx_hash"],
+                    "tx_index": tx["tx_index"],
+                },
+            },
+            {
+                "table": "debits",
+                "values": {
+                    "action": "open order",
+                    "address": defaults["addresses"][0],
+                    "asset": "MAXI",
+                    "block_index": current_block_index,
+                    "event": tx["tx_hash"],
+                    "quantity": 9223372036854775807,
+                },
+            },
+        ],
+    )
+
+
+def test_parse_more_than_maxi(ledger_db, blockchain_mock, defaults, caplog, test_helpers):
+    tx = blockchain_mock.dummy_tx(ledger_db, defaults["addresses"][0])
+    message = b"\x00\x00\x00\x00\x00\x03:>\x7f\xff\xff\xff\xff\xff\xff\xff\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x05\xf5\xe1\x00\x00\n\x80\x00\x00\x00\x00\x00\x00\x00"
+
+    with test_helpers.capture_log(caplog, "invalid: integer overflow"):
+        order.parse(ledger_db, tx, message)
