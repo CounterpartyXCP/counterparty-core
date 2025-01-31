4378c7ecbc5e66c96d0230476faa0bf45e8bf229
Ouziel Slama <ouziel@gmail.com>
2025-01-31 17:12:54 +0000

    fix typo

--- counterparty-core/counterpartycore/regtest/scenarios_test.py
+++ counterparty-core/counterpartycore/regtest/scenarios_test.py
@@ -70,7 +70,7 @@ SCENARIOS += scenario_25_issuance.SCENARIO
 SCENARIOS += scenario_last_mempool.SCENARIO
 
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
-BASE_DIR = os.path.join(CURR_DIR, "../../../../")
+BASE_DIR = os.path.join(CURR_DIR, "../../../")
 
 # SCENARIOS = scenario_25_issuance.SCENARIO
 
@@ -418,23 +418,22 @@ def run_scenarios(serve=False, wsgi_server="gunicorn"):
                 )
                 time.sleep(1)
         else:
-            if True:
-                print("Generating API documentation...")
-                if os.path.exists(os.path.join(CURR_DIR, "apidoc/apicache.json")):
-                    os.unlink(os.path.join(CURR_DIR, "apidoc/apicache.json"))
-                sh.python3(
-                    os.path.join(CURR_DIR, "genapidoc.py"),
-                    os.path.abspath("regtestnode"),
-                    _out=sys.stdout,
-                    _err_to_out=True,
-                    _cwd=CURR_DIR,
-                )
-                print("Running Dredd...")
-                sh.dredd(
-                    _cwd=BASE_DIR,
-                    _out=sys.stdout,
-                    _err_to_out=True,
-                )
+            print("Generating API documentation...")
+            if os.path.exists(os.path.join(CURR_DIR, "apidoc/apicache.json")):
+                os.unlink(os.path.join(CURR_DIR, "apidoc/apicache.json"))
+            sh.python3(
+                os.path.join(CURR_DIR, "genapidoc.py"),
+                os.path.abspath("regtestnode"),
+                _out=sys.stdout,
+                _err_to_out=True,
+                _cwd=CURR_DIR,
+            )
+            print("Running Dredd...")
+            sh.dredd(
+                _cwd=BASE_DIR,
+                _out=sys.stdout,
+                _err_to_out=True,
+            )
             print("Testing invalid detach...")
             regtest_node_thread.node.test_invalid_detach()
             print("Testing transaction chaining...")
