f2133f7e3917fed983cc62f811c8a8b888f9122a
Ouziel Slama <ouziel@gmail.com>
2025-02-03 17:55:11 +0000

    more time for github; tweaks

--- counterparty-core/counterpartycore/test/integrations/compose_test.py
+++ counterparty-core/counterpartycore/test/integrations/compose_test.py
@@ -4,6 +4,7 @@ import tempfile
 import time
 from io import StringIO
 
+import requests
 import sh
 
 
@@ -17,35 +18,58 @@ def print_docker_output(out, printed_line_count):
     return printed_line_count, "\n".join(printed_line)
 
 
+def rpc_call(url, method, params):
+    import json
+
+    headers = {"content-type": "application/json"}
+    payload = {
+        "method": method,
+        "params": params,
+        "jsonrpc": "2.0",
+        "id": 0,
+    }
+    return requests.post(
+        url,
+        data=json.dumps(payload),
+        headers=headers,
+        timeout=30,
+    ).json()
+
+
 def test_docker_compose():
     sh.docker("system", "prune", "--all", "--force", _out=sys.stdout, _err_to_out=True)
 
-    DATA_DIR = os.path.join(tempfile.gettempdir(), "counterparty-docker-data")
-    print(f"DATA_DIR: {DATA_DIR}")
-    if os.path.exists(DATA_DIR):
-        with sh.contrib.sudo(password="", _with=True):
-            sh.rm("-rf", DATA_DIR)
-    os.makedirs(DATA_DIR)
+    try:
+        DATA_DIR = os.path.join(tempfile.gettempdir(), "counterparty-docker-data")
+        print(f"DATA_DIR: {DATA_DIR}")
+        if os.path.exists(DATA_DIR):
+            with sh.contrib.sudo(password="", _with=True):
+                sh.rm("-rf", DATA_DIR)
+        os.makedirs(DATA_DIR)
 
-    out = StringIO()
-    sh.docker(
-        "compose",
-        "--profile",
-        "regtest",
-        "up",
-        _bg=True,
-        _out=out,
-        _err_to_out=True,
-        _env={"COUNTERPARTY_DOCKER_DATA": DATA_DIR},
-    )
-    printed_line_count = 0
-    start_time = time.time()
-    while True:
-        printed_line_count, printed_line = print_docker_output(out, printed_line_count)
-        if "Ledger.Main - Watching for new blocks..." in printed_line:
-            break
-        if time.time() - start_time > 60 * 5:
-            raise Exception("Timeout: not ready after 5 minutes")
-        time.sleep(0.1)
-
-    sh.docker("compose", "--profile", "regtest", "stop", _out=sys.stdout, _err_to_out=True)
+        out = StringIO()
+        sh.docker(
+            "compose",
+            "--profile",
+            "regtest",
+            "up",
+            _bg=True,
+            _out=out,
+            _err_to_out=True,
+            _env={"COUNTERPARTY_DOCKER_DATA": DATA_DIR},
+        )
+        printed_line_count = 0
+        start_time = time.time()
+        while True:
+            printed_line_count, printed_line = print_docker_output(out, printed_line_count)
+            if "Ledger.Main - Watching for new blocks..." in printed_line:
+                break
+            if time.time() - start_time > 60 * 20:
+                raise Exception("Timeout: not ready after 5 minutes")
+            time.sleep(0.1)
+    finally:
+        try:
+            sh.docker("compose", "--profile", "regtest", "stop", _out=sys.stdout, _err_to_out=True)
+        except sh.ErrorReturnCode:
+            pass
+        sh.docker("system", "prune", "--all", "--force", _out=sys.stdout, _err_to_out=True)
