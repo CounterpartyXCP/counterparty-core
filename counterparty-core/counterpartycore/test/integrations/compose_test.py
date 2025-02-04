e906ec8e3aa298db44a99eb1b857fa5c4e5b23de
Ouziel Slama <ouziel@gmail.com>
2025-02-04 08:22:17 +0000

    tweak docker compose test

--- counterparty-core/counterpartycore/test/integrations/compose_test.py
+++ counterparty-core/counterpartycore/test/integrations/compose_test.py
@@ -69,7 +69,26 @@ def test_docker_compose():
                 assert result["result"]["network"] == "regtest"
                 break
             if time.time() - start_time > 60 * 20:
-                raise Exception("Timeout: not ready after 5 minutes")
+                raise Exception("Timeout: not ready after 20 minutes")
+            time.sleep(1)
+        sh.docker(
+            "exec",
+            "counterparty-core-bitcoind-regtest-1",
+            "bitcoin-cli",
+            "-regtest",
+            "-rpcuser=rpc",
+            "-rpcpassword=rpc",
+            "generatetoaddress",
+            120,
+            "bcrt1q6e4xyptaygg23m2sux2arnpj7j4x864md43pw8",
+        )
+        start_time = time.time()
+        while True:
+            printed_line_count, printed_line = print_docker_output(out, printed_line_count)
+            if "Block 120 - Parsing complete." in printed_line:
+                break
+            if time.time() - start_time > 60 * 20:
+                raise Exception("Timeout: not ready after 20 minutes")
             time.sleep(1)
     finally:
         try:
