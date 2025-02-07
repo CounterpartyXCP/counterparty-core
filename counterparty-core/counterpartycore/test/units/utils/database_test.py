51bd6040f87c39678098c69011072b668d4050e0
Ouziel Slama <ouziel@gmail.com>
2025-02-07 13:45:42 +0000

    fix tests

--- counterparty-core/counterpartycore/test/units/utils/database_test.py
+++ counterparty-core/counterpartycore/test/units/utils/database_test.py
@@ -3,8 +3,6 @@ from counterpartycore.lib.utils import database
 
 
 def test_version(ledger_db, test_helpers):
-    assert database.version(ledger_db) == (0, 0)
-
     database.update_version(ledger_db)
     test_helpers.check_records(
         ledger_db,
