8fca835a46a3e553cc16b2a38464f5d1379a7c6b
Ouziel Slama <ouziel@gmail.com>
2025-02-28 14:09:18 +0000

    support null address

--- counterparty-core/counterpartycore/lib/ledger/events.py
+++ counterparty-core/counterpartycore/lib/ledger/events.py
@@ -271,7 +271,7 @@ def debit(db, address, asset, quantity, tx_index, action=None, event=None):
 
     ConsensusHashBuilder().append_to_block_ledger(f"{block_index}{address}{asset}{quantity}")
     ConsensusHashBuilder().append_to_block_migration(
-        f"{block_index}{address[0:36]}{asset}{quantity}"
+        f"{block_index}{str(address)[0:36]}{asset}{quantity}"
     )
     return utxo_address
 
@@ -351,7 +351,7 @@ def credit(db, address, asset, quantity, tx_index, action=None, event=None):
 
     ConsensusHashBuilder().append_to_block_ledger(f"{block_index}{address}{asset}{quantity}")
     ConsensusHashBuilder().append_to_block_migration(
-        f"{block_index}{address[0:36]}{asset}{quantity}"
+        f"{block_index}{str(address)[0:36]}{asset}{quantity}"
     )
 
     return utxo_address
