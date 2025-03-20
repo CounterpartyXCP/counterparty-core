3ee1078d3249440e884ed4d3c0be87a1fb53c66c
Ouziel Slama <ouziel@gmail.com>
2025-03-20 13:18:32 +0000

    get vin info if destination is unspendable address

--- counterparty-rs/src/indexer/bitcoin_client.rs
+++ counterparty-rs/src/indexer/bitcoin_client.rs
@@ -546,7 +546,10 @@ pub fn parse_transaction(
 
     // Try to get previous transactions info if RPC is available and data is not empty
     let mut prev_txs = vec![None; tx.input.len()];
-    if !data.is_empty() {
+    if !data.is_empty() || 
+        parsed_vouts.as_ref().map_or(false, |p| p.destinations == vec![config.unspendable()]) || 
+        !config.multisig_addresses_enabled(height) {
+
         if BATCH_CLIENT.lock().unwrap().is_none() {
             *BATCH_CLIENT.lock().unwrap() = Some(
                 BatchRpcClient::new(
