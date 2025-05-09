8dc8e17342d066e884ff4f3026ac671239dcf0c4
Ouziel Slama <ouziel@gmail.com>
2025-05-09 20:29:32 +0000

    fix sighash check

--- counterparty-core/counterpartycore/lib/parser/gettxinfo.py
+++ counterparty-core/counterpartycore/lib/parser/gettxinfo.py
@@ -177,18 +177,13 @@ def collect_sighash_flags(script_sig, witnesses):
             flags.append(flag)
         return flags
 
-    # P2TR key path spend
-    if len(witnesses) == 1:
+    # P2TR
+    if len(witnesses) == 1 or len(witnesses) == 3:
         flag = get_schnorr_signature_sighash_flag(witnesses[0])
         if flag is not None:
             flags.append(flag)
         return flags
 
-    # Other cases
-    for item in witnesses:
-        flag = get_schnorr_signature_sighash_flag(item) or get_der_signature_sighash_flag(item)
-        if flag is not None:
-            flags.append(flag)
     return flags
 
 
